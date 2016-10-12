a495667229a78aff611e2af8f222a18b3d6e0251
Masahiro Fujiwara <m-fujiwara@axsh.net>
2016-10-12 22:58:31 +0900

    Add a dummy scheduler.

---
+++ scheduler/main.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/gogo/protobuf/proto"
+	"github.com/mesos/mesos-go/auth"
+	"github.com/mesos/mesos-go/auth/sasl"
+	mesos "github.com/mesos/mesos-go/mesosproto"
+	util "github.com/mesos/mesos-go/mesosutil"
+	sched "github.com/mesos/mesos-go/scheduler"
+	"golang.org/x/net/context"
+)
+
+const (
+	CPUS_PER_EXECUTOR = 0.01
+	CPUS_PER_TASK     = 1
+	MEM_PER_EXECUTOR  = 64
+	MEM_PER_TASK      = 64
+)
+
+type VDCScheduler struct {
+	executor *mesos.ExecutorInfo
+}
+
+func newVDCScheduler() *VDCScheduler {
+	executorCommand := fmt.Sprintf("./%s -logtostderr=true -v=%d -slow_tasks=%v", "./executor", 0, false)
+	uri := ""
+	executorUris := []*mesos.CommandInfo_URI{
+		&mesos.CommandInfo_URI{Value: &uri, Executable: proto.Bool(true)},
+	}
+
+	exec := &mesos.ExecutorInfo{
+		ExecutorId: util.NewExecutorID("default"),
+		Name:       proto.String("Test Executor (Go)"),
+		Source:     proto.String("go_test"),
+		Command: &mesos.CommandInfo{
+			Value: proto.String(executorCommand),
+			Uris:  executorUris,
+		},
+		Resources: []*mesos.Resource{
+			util.NewScalarResource("cpus", CPUS_PER_EXECUTOR),
+			util.NewScalarResource("mem", MEM_PER_EXECUTOR),
+		},
+	}
+	return &VDCScheduler{executor: exec}
+}
+func (sched *VDCScheduler) Registered(driver sched.SchedulerDriver, frameworkId *mesos.FrameworkID, masterInfo *mesos.MasterInfo) {
+	log.Println("Framework Registered with Master ", masterInfo)
+}
+
+func (sched *VDCScheduler) Reregistered(driver sched.SchedulerDriver, masterInfo *mesos.MasterInfo) {
+	log.Println("Framework Re-Registered with Master ", masterInfo)
+}
+
+func (sched *VDCScheduler) Disconnected(sched.SchedulerDriver) {
+	log.Println("disconnected from master")
+}
+
+func (sched *VDCScheduler) ResourceOffers(driver sched.SchedulerDriver, offers []*mesos.Offer) {
+	log.Println("Framework Resource Offers from master", offers)
+}
+
+func (sched *VDCScheduler) StatusUpdate(driver sched.SchedulerDriver, status *mesos.TaskStatus) {
+	log.Println("Framework Resource Offers from master", status)
+}
+
+func (sched *VDCScheduler) OfferRescinded(_ sched.SchedulerDriver, oid *mesos.OfferID) {
+	log.Fatalf("offer rescinded: %v", oid)
+}
+
+func (sched *VDCScheduler) FrameworkMessage(_ sched.SchedulerDriver, eid *mesos.ExecutorID, sid *mesos.SlaveID, msg string) {
+	log.Fatalf("framework message from executor %q slave %q: %q", eid, sid, msg)
+}
+
+func (sched *VDCScheduler) SlaveLost(_ sched.SchedulerDriver, sid *mesos.SlaveID) {
+	log.Fatalf("slave lost: %v", sid)
+}
+
+func (sched *VDCScheduler) ExecutorLost(_ sched.SchedulerDriver, eid *mesos.ExecutorID, sid *mesos.SlaveID, code int) {
+	log.Fatalf("executor %q lost on slave %q code %d", eid, sid, code)
+}
+func (sched *VDCScheduler) Error(_ sched.SchedulerDriver, err string) {
+	log.Fatalf("Scheduler received error: %v", err)
+}
+
+func main() {
+	fwinfo := &mesos.FrameworkInfo{
+		User: proto.String(""), // Mesos-go will fill in user.
+		Name: proto.String("VDC Scheduler"),
+	}
+	cred := &mesos.Credential{
+		Principal: proto.String(""),
+		Secret:    proto.String(""),
+	}
+	bindingAddr := net.ParseIP("127.0.0.1")
+	config := sched.DriverConfig{
+		Scheduler:      newVDCScheduler(),
+		Framework:      fwinfo,
+		Master:         "127.0.0.1:5050",
+		Credential:     cred,
+		BindingAddress: bindingAddr,
+		WithAuthContext: func(ctx context.Context) context.Context {
+			ctx = auth.WithLoginProvider(ctx, sasl.ProviderName)
+			ctx = sasl.WithBindingAddress(ctx, bindingAddr)
+			return ctx
+		},
+	}
+	driver, err := sched.NewMesosSchedulerDriver(config)
+	if err != nil {
+		log.Fatalln("Unable to create a SchedulerDriver ", err.Error())
+	}
+	if stat, err := driver.Run(); err != nil {
+		log.Println("Framework stopped with status %s and error: %s\n", stat.String(), err.Error())
+	}
+}
