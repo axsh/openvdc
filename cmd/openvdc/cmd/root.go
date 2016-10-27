20b834f710b28602308278c0281daf2ac9915695
Masahiro Fujiwara <m-fujiwara@axsh.net>
2016-10-27 22:23:25 +0900

    Add openvdc CLI stub based on "github.com/spf13/cobra".

---
+++ cmd/openvdc/cmd/root.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var cfgFile string
+
+// RootCmd represents the base command when called without any subcommands
+var RootCmd = &cobra.Command{
+	Use:   "openvdc",
+	Short: "",
+	Long:  ``,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports Persistent Flags, which, if defined here,
+	// will be global for your application.
+
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.openvdc.yaml)")
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cmd/openvdc/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/axsh/openvdc/cmd/openvdc/cmd"
+
+func main() {
+	cmd.Execute()
+}
