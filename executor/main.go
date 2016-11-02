268a9a7c9309056fb619db39c3b7a0ed1f9f285f
b0r6 <tommy@axsh.net>
2016-11-02 14:33:27 +0900

    Print to log file

--- executor/main.go
+++ executor/main.go
@@ -38,22 +38,22 @@ func newVDCExecutor() *VDCExecutor {
 }
 
 func (exec *VDCExecutor) Registered(driver exec.ExecutorDriver, execInfo *mesos.ExecutorInfo, fwinfo *mesos.FrameworkInfo, slaveInfo *mesos.SlaveInfo) {
-	fmt.Println("Registered Executor on slave ", slaveInfo.GetHostname())
+	log.Println("Registered Executor on slave ", slaveInfo.GetHostname())
 }
 
 func (exec *VDCExecutor) Reregistered(driver exec.ExecutorDriver, slaveInfo *mesos.SlaveInfo) {
-	fmt.Println("Re-registered Executor on slave ", slaveInfo.GetHostname())
+	log.Println("Re-registered Executor on slave ", slaveInfo.GetHostname())
 }
 
 func (exec *VDCExecutor) Disconnected(driver exec.ExecutorDriver) {
-	fmt.Println("Executor disconnected.")
+	log.Println("Executor disconnected.")
 }
 
 func zkConnect(ip string) *zk.Conn {
 	c, _, err := zk.Connect([]string{ip}, time.Second)
 
 	if err != nil {
-		fmt.Println("VDCExecutor failed connecting to Zookeeper: ", err)
+		log.Println("VDCExecutor failed connecting to Zookeeper: ", err)
 	}
 
 	return c
@@ -63,10 +63,10 @@ func zkGetData(c *zk.Conn, dir string) []byte {
 	data, stat, err := c.Get(dir)
 
 	if err != nil {
-		fmt.Println("VDCExecutor failed getting data from Zookeeper: ", err)
+		log.Println("VDCExecutor failed getting data from Zookeeper: ", err)
 	}
 
-	fmt.Println(stat)
+	log.Println(stat)
 
 	return data[:]
 }
@@ -78,11 +78,11 @@ func zkSendData(c *zk.Conn, dir string, data string) {
 	path, err := c.Create(dir, []byte(data), flags, acl)
 
 	if err != nil {
-		fmt.Println("VDCExecutor failed sending data to Zookeeper: ", err)
+		log.Println("VDCExecutor failed sending data to Zookeeper: ", err)
 	}
 
-	fmt.Println("Sent: ", data, "to ", dir)
-	fmt.Println(path)
+	log.Println("Sent: ", data, "to ", dir)
+	log.Println(path)
 }
 
 func testZkConnection(ip string, dir string, msg string) {
@@ -90,17 +90,17 @@ func testZkConnection(ip string, dir string, msg string) {
 	c := zkConnect(ip)
 	zkSendData(c, dir, msg)
 	data := []byte(zkGetData(c, dir))
-	fmt.Println(data)
+	log.Println(data)
 }
 
 func newLxcContainer() {
 
 	c, err := lxc.NewContainer(name, lxcpath)
 	if err != nil {
-		fmt.Println("ERROR: %s\n", err.Error())
+		log.Println("ERROR: %s\n", err.Error())
 	}
 
-	fmt.Println("Creating lxc-container...\n")
+	log.Println("Creating lxc-container...\n")
 	if verbose {
 		c.SetVerbosity(lxc.Verbose)
 	}
@@ -115,7 +115,7 @@ func newLxcContainer() {
 	}
 
 	if err := c.Create(options); err != nil {
-		fmt.Println("ERROR: %s\n", err.Error())
+		log.Println("ERROR: %s\n", err.Error())
 	}
 }
 
@@ -123,12 +123,12 @@ func destroyLxcContainer() {
 
 	c, err := lxc.NewContainer(name, lxcpath)
         if err != nil {
-                fmt.Println("ERROR: %s\n", err.Error())
+                log.Println("ERROR: %s\n", err.Error())
         }
 
-	fmt.Println("Destroying lxc-container...\n")
+	log.Println("Destroying lxc-container...\n")
 	if err := c.Destroy(); err != nil {
-		fmt.Println("ERROR: %s\n", err.Error())
+		log.Println("ERROR: %s\n", err.Error())
 	}
 }
 
@@ -136,17 +136,17 @@ func startLxcContainer() {
 
 	c, err := lxc.NewContainer(name, lxcpath)
         if err != nil {
-                fmt.Println("ERROR: %s\n", err.Error())
+                log.Println("ERROR: %s\n", err.Error())
         }
 
-	fmt.Println("Starting lxc-container...\n")
+	log.Println("Starting lxc-container...\n")
 	if err := c.Start(); err != nil {
-		fmt.Println("ERROR: %s\n", err.Error())
+		log.Println("ERROR: %s\n", err.Error())
 	}
 
-	fmt.Println("Waiting for lxc-container to start networking\n")
+	log.Println("Waiting for lxc-container to start networking\n")
 	if _, err := c.WaitIPAddresses(5 * time.Second); err != nil {
-		fmt.Println("ERROR: %s\n", err.Error())
+		log.Println("ERROR: %s\n", err.Error())
 	}
 }
 
@@ -154,12 +154,12 @@ func stopLxcContainer() {
 
 	c, err := lxc.NewContainer(name, lxcpath)
         if err != nil {
-                fmt.Println("ERROR: %s\n", err.Error())
+                log.Println("ERROR: %s\n", err.Error())
         }
 
-	fmt.Println("Stopping lxc-container...\n")
+	log.Println("Stopping lxc-container...\n")
 	if err := c.Stop(); err != nil {
-		fmt.Println("ERROR: %s\n", err.Error())
+		log.Println("ERROR: %s\n", err.Error())
 	}
 }
 
@@ -176,25 +176,25 @@ func newTask(taskName string) {
 
         switch trimmedTaskName {
                 case "lxc-create":
-			fmt.Println("---Launching task: lxc-create---")
+			log.Println("---Launching task: lxc-create---")
                         newLxcContainer()
                 case "lxc-start":
-			fmt.Println("---Launching task: lxc-start---")
+			log.Println("---Launching task: lxc-start---")
                         startLxcContainer(lxc)
                 case "lxc-stop":
-			fmt.Println("---Launching task: lxc-stop---")
+			log.Println("---Launching task: lxc-stop---")
                         stopLxcContainer(lxc)
                 case "lxc-destroy":
-			fmt.Println("---Launching task: lxc-destroy---")
+			log.Println("---Launching task: lxc-destroy---")
                         destroyLxcContainer(lxc)
                 default:
-                        fmt.Println("ERROR: Taskname unrecognized")
+                        log.Println("ERROR: Taskname unrecognized")
         }
 }
 
 
 func (exec *VDCExecutor) LaunchTask(driver exec.ExecutorDriver, taskInfo *mesos.TaskInfo) {
-	fmt.Println("Launching task", taskInfo.GetName(), "with command", taskInfo.Command.GetValue())
+	log.Println("Launching task", taskInfo.GetName(), "with command", taskInfo.Command.GetValue())
 
 	runStatus := &mesos.TaskStatus{
 		TaskId: taskInfo.GetTaskId(),
@@ -202,26 +202,26 @@ func (exec *VDCExecutor) LaunchTask(driver exec.ExecutorDriver, taskInfo *mesos.
 	}
 	_, err := driver.SendStatusUpdate(runStatus)
 	if err != nil {
-		fmt.Println("Got error", err)
+		log.Println("Got error", err)
 	}
 
 	exec.tasksLaunched++
-	fmt.Println("Total tasks launched ", exec.tasksLaunched)
+	log.Println("Total tasks launched ", exec.tasksLaunched)
 
 
 	newTask(taskInfo.GetName())
 
 
 	finishTask := func() {
-		fmt.Println("Finishing task", taskInfo.GetName())
+		log.Println("Finishing task", taskInfo.GetName())
 		finStatus := &mesos.TaskStatus{
 			TaskId: taskInfo.GetTaskId(),
 			State:  mesos.TaskState_TASK_FINISHED.Enum(),
 		}
 		if _, err := driver.SendStatusUpdate(finStatus); err != nil {
-			fmt.Println("error sending FINISHED", err)
+			log.Println("error sending FINISHED", err)
 		}
-		fmt.Println("Task finished", taskInfo.GetName())
+		log.Println("Task finished", taskInfo.GetName())
 	}
 	if *slowTasks {
 		starting := &mesos.TaskStatus{
@@ -229,7 +229,7 @@ func (exec *VDCExecutor) LaunchTask(driver exec.ExecutorDriver, taskInfo *mesos.
 			State:  mesos.TaskState_TASK_STARTING.Enum(),
 		}
 		if _, err := driver.SendStatusUpdate(starting); err != nil {
-			fmt.Println("error sending STARTING", err)
+			log.Println("error sending STARTING", err)
 		}
 		delay := time.Duration(rand.Intn(90)+10) * time.Second
 		go func() {
@@ -242,19 +242,19 @@ func (exec *VDCExecutor) LaunchTask(driver exec.ExecutorDriver, taskInfo *mesos.
 }
 
 func (exec *VDCExecutor) KillTask(driver exec.ExecutorDriver, taskID *mesos.TaskID) {
-	fmt.Println("Kill task")
+	log.Println("Kill task")
 }
 
 func (exec *VDCExecutor) FrameworkMessage(driver exec.ExecutorDriver, msg string) {
-	fmt.Println("Got framework message: ", msg)
+	log.Println("Got framework message: ", msg)
 }
 
 func (exec *VDCExecutor) Shutdown(driver exec.ExecutorDriver) {
-	fmt.Println("Shutting down the executor")
+	log.Println("Shutting down the executor")
 }
 
 func (exec *VDCExecutor) Error(driver exec.ExecutorDriver, err string) {
-	fmt.Println("Got error message:", err)
+	log.Println("Got error message:", err)
 }
 
 func init() {
@@ -301,7 +301,10 @@ func setupLog(logpath string, filename string, prefix string){
 }
 
 func main() {
-	fmt.Println("Starting VDC Executor (Go)")
+
+	setupLog("/var/log/axsh/", "vdc-executor.log", "VDC-EXECUTOR: ")
+
+	log.Println("Starting VDC Executor (Go)")
 
 	dconfig := exec.DriverConfig{
 		Executor: newVDCExecutor(),
@@ -309,19 +312,19 @@ func main() {
 	driver, err := exec.NewMesosExecutorDriver(dconfig)
 
 	if err != nil {
-		fmt.Println("Unable to create a ExecutorDriver ", err.Error())
+		log.Println("Unable to create a ExecutorDriver ", err.Error())
 	}
 
 	_, err = driver.Start()
 	if err != nil {
-		fmt.Println("Got error:", err)
+		log.Println("Got error:", err)
 		return
 	}
-	fmt.Println("Executor process has started and running.")
+	log.Println("Executor process has started and running.")
 
 	_, err = driver.Join()
 	if err != nil {
-		fmt.Println("driver failed:", err)
+		log.Println("driver failed:", err)
 	}
-	fmt.Println("executor terminating")
+	log.Println("executor terminating")
 }
