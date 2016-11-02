d3da09ea80286e18ddfcb69ceb1fc36afcbb3e6b
Masahiro Fujiwara <m-fujiwara@axsh.net>
2016-11-02 20:28:25 +0900

    Replaced the cli binary to openvdc CLI.

--- cmd/openvdc/cmd/root.go
+++ cmd/openvdc/cmd/root.go
@@ -22,6 +22,7 @@ var RootCmd = &cobra.Command{
 // Execute adds all child commands to the root command sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	RootCmd.AddCommand(runCmd)
 	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
---
+++ cmd/openvdc/cmd/run.go
@@ -0,0 +1,40 @@
+package cmd
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/axsh/openvdc/proto"
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
+)
+
+var serverAddr string
+
+func init() {
+	runCmd.PersistentFlags().StringVarP(&serverAddr, "server", "s", "localhost:5000", "gRPC API server address")
+	runCmd.PersistentFlags().SetAnnotation("server", cobra.BashCompSubdirsInDir, []string{})
+}
+
+var runCmd = &cobra.Command{
+	Use:   "run [Image ID]",
+	Short: "Run an instance",
+	Long:  `Register and start new instance.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("Connection error: %v", err)
+		}
+
+		defer conn.Close()
+
+		c := pb.NewInstanceClient(conn)
+
+		resp, err := c.Run(context.Background(), &pb.RunRequest{})
+		if err != nil {
+			log.Fatalf("RPC error: %v", err)
+		}
+		log.Println(resp)
+		return nil
+	},
+}
