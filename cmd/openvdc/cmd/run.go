48d83c7d4c327312fd90d2d0a7365042a5e468d8
b0r6 <tommy@axsh.net>
2016-11-09 18:36:25 +0900

    Updated client

--- cmd/openvdc/cmd/run.go
+++ cmd/openvdc/cmd/run.go
@@ -3,7 +3,7 @@ package cmd
 import (
 	"context"
 	"log"
-	"fmt"
+	"strings"
 
 	pb "github.com/axsh/openvdc/proto"
 	util "github.com/axsh/openvdc/util"
@@ -17,37 +17,51 @@ var hostName string
 
 func init() {
 	runCmd.PersistentFlags().StringVarP(&serverAddr, "server", "s", "localhost:5000", "gRPC API server address")
-	runCmd.PersistentFlags().StringVarP(&imageName,"image", "i", "centos7", "Name of image file")
         runCmd.PersistentFlags().StringVarP(&hostName,"name", "n", "", "Existing host name")
-	runCmd.PersistentFlags().SetAnnotation("server", cobra.BashCompSubdirsInDir, []string{})
+        runCmd.PersistentFlags().SetAnnotation("server", cobra.BashCompSubdirsInDir, []string{})
 }
 
 var runCmd = &cobra.Command{
-	Use:   "run [Image ID]",
-	Short: "Run an instance",
-	Long:  `Register and start new instance.`,
-	RunE: func(cmd *cobra.Command, args []string) error {
+        Use:   "run [Image ID]",
+        Short: "Run an instance",
+        Long:  `Register and start new instance.`,
+        RunE: func(cmd *cobra.Command, args []string) error {
 
-		result := util.GetRemoteJsonField("variables.memory", "https://raw.githubusercontent.com/axsh/openvdc/master/deployment/1box/1box-centos7.json")
+                imageTitle := strings.ToLower(util.GetRemoteJsonField("title", "https://raw.githubusercontent.com/openvdc/images/master/centos-7.json"))
+                imageUrl := util.GetRemoteJsonField("images.download_url", "https://raw.githubusercontent.com/openvdc/images/master/centos-7.json")
 
-		fmt.Println("Json: ", result)
-                fmt.Println("Image name: ", imageName)
-                fmt.Println("Host name: ", hostName)
+                if(len(args) > 0 ){
+                        inputImageTitle := args[0]
 
-		conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
-		if err != nil {
-			log.Fatalf("Connection error: %v", err)
-		}
+                        if inputImageTitle  ==  imageTitle {
 
-		defer conn.Close()
+                                log.Println("Found image: ", imageTitle)
+                                log.Println("ImageUrl: ", imageUrl)
 
-		c := pb.NewInstanceClient(conn)
+                                //Viper isn't able to fetch the field "images.download_url" right now. Probably due to square brackets in the Json-file.
+                                imageUrl = "https://images.linuxcontainers.org/1.0/images/d767cfe9a0df0b2213e28b39b61e8f79cb9b1e745eeed98c22bc5236f277309a/export"
 
-		resp, err := c.Run(context.Background(), &pb.RunRequest{imageName,hostName})
-		if err != nil {
-			log.Fatalf("RPC error: %v", err)
-		}
-		log.Println(resp)
-		return nil
-	},
-}
+                                log.Println(imageUrl)
+
+                                conn, err := grpc.Dial(serverAddr, grpc.WithInsecure())
+                                if err != nil {
+                                        log.Fatalf("ERROR: Cannot connect to OpenVDC API: %v", err)
+                                }
+
+                                defer conn.Close()
+
+                                c := pb.NewInstanceClient(conn)
+
+                                resp, err := c.Run(context.Background(), &pb.RunRequest{imageTitle,imageUrl,hostName})
+
+                                if err != nil {
+                                        log.Fatalf("ERROR: Cannot connect to OpenVDC API: %v", err)
+                                }
+
+                                log.Println(resp)
+			} else { log.Warn("OpenVDC: Image not found. Available images: centos7")  }
+                } else {
+                        log.Warn("OpenVDC: Please provide an Image ID.  Usage: run [Image ID]")
+        }
+                return nil
+}}
