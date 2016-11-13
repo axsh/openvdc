61cfd5b6b0de4cd56ee4e965e18eb8a22f80af0b
Masahiro Fujiwara <m-fujiwara@axsh.net>
2016-11-13 20:36:33 +0900

    Add bash version of build script to compile .proto.

---
+++ proto/protoc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+if ! type protoc ; then
+  echo "Can not find protoc. Download pre-compiled binary from https://github.com/google/protobuf/releases"
+  exit 1
+fi
+if ! type protoc-gen-go; then
+  go get -u -v github.com/golang/protobuf/protoc-gen-go
+fi
+protoc -I . --go_out=plugins=grpc:. v1.proto
