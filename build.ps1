15ba552cb7aa8236592a9d1c129f7592ce9d4a43
Masahiro Fujiwara <m-fujiwara@axsh.net>
2016-11-14 15:47:12 +0900

    Update build.ps1 to embed build-time variables same as bb95eb41.

--- build.ps1
+++ build.ps1
@@ -1,4 +1,13 @@
-go build -v ./cmd/openvdc
-go build -v ./cmd/openvdc-scheduler
-go build -v ./cmd/openvdc-executor
+
+$VERSION="dev"
+$SHA=$(git rev-parse --verify HEAD)
+$BUILDDATE=Get-Date -Format "yyyy/MM/dd HH:mm:ss zzz"
+$GOVERSION=$(go version)
+$LDFLAGS="-X 'main.version=${VERSION}' -X 'main.sha=${SHA}' -X 'main.builddate=${BUILDDATE}' -X 'main.goversion=${GOVERSION}'"
+# During development, assume that the executor binary locates in the build directory.
+$EXECUTOR_PATH=Join-Path $(Get-Location) "openvdc-executor"
+
+go build -ldflags "$LDFLAGS" -v ./cmd/openvdc
+go build -ldflags "$LDFLAGS" -ldflags "-X 'github.com/axsh/openvdc/scheduler.ExecutorPath=${EXECUTOR_PATH}'" -v ./cmd/openvdc-scheduler
+go build -ldflags "$LDFLAGS" -v ./cmd/openvdc-executor
 Write-Host "Done"
