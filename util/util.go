9fca74bdbb656ba95ce7c003a9b0a6de77a5852e
Masahiro Fujiwara <m-fujiwara@axsh.net>
2016-11-15 20:17:23 +0900

    Remove unused method.

--- util/util.go
+++ util/util.go
@@ -1,32 +1,13 @@
 package util
 
 import (
-	"net/http"
 	"os"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/axsh/openvdc/api"
-	"github.com/spf13/viper"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
-
-func GetRemoteJsonField(field string, url string) string {
-	r, err := http.Get(url)
-
-	if err != nil {
-		log.Fatal("Couldn't fetch remote JSON: ", err)
-	} else {
-		defer r.Body.Close()
-	}
-
-	viper.SetConfigType("json")
-	viper.ReadConfig(r.Body)
-
-	result := viper.GetString(field)
-
-	return result
-}
 
 func SetupLog() {
 	log.SetOutput(os.Stdout)
