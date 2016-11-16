c6db9bf1e320f3fea7c994b4a93f36d6341178ac
Masahiro Fujiwara <m-fujiwara@axsh.net>
2016-11-16 16:05:02 +0900

    model: Create unique root path for every test cases.

--- model/backend/zk_test.go
+++ model/backend/zk_test.go
@@ -3,23 +3,43 @@ package backend
 import (
 	"testing"
 
+	"github.com/pborman/uuid"
 	"github.com/stretchr/testify/assert"
 )
 
+var testZkServer = "127.0.0.1"
+
+func withConnect(t *testing.T, c func(z *Zk)) (err error) {
+	z := NewZkBackend()
+	z.basePath = "/" + uuid.New()
+	defer func() {
+		err = z.conn.Delete(z.basePath, 1)
+		z.Close()
+	}()
+	err = z.Connect([]string{testZkServer})
+	if err != err {
+		t.Fatal(err)
+	}
+	c(z)
+	return
+}
+
 func TestZkConnect(t *testing.T) {
 	assert := assert.New(t)
-	z := NewZkBackend()
-	defer z.Close()
-	err := z.Connect([]string{"192.168.56.120"})
-	assert.NoError(err)
+	withConnect(t, func(z *Zk) {
+		assert.NotNil(z)
+		assert.NotNil(z.conn)
+	})
 }
 
 func TestZkCreate(t *testing.T) {
 	assert := assert.New(t)
-	z := NewZkBackend()
-	defer z.Close()
-	err := z.Connect([]string{"192.168.56.120"})
-	assert.NoError(err)
-	err = z.Create("/test1", []byte{})
-	assert.NoError(err)
+	withConnect(t, func(z *Zk) {
+		err := z.Create("/test1", []byte{})
+		assert.NoError(err)
+		err = z.Create("/test1", []byte{})
+		assert.Error(err)
+		// TODO: implemente ModelBackend.Delete().
+		z.conn.Delete(z.basePath+"/test1", 1)
+	})
 }
