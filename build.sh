b94a9566857e35e8c9b482a419b8a355f1d5d0b0
akirapop <cbaluta20@gmail.com>
2016-11-16 16:37:02 +0900

    - Added code to download govendor - Added code to sync via govendor

--- build.sh
+++ build.sh
@@ -10,6 +10,9 @@ LDFLAGS="-X 'main.version=${VERSION}' -X 'main.sha=${SHA}' -X 'main.builddate=${
 # During development, assume that the executor binary locates in the build directory.
 EXECUTOR_PATH=$(pwd)/openvdc-executor
 
+go get -u github.com/kardianos/govendor
+govendor sync
+
 go build -ldflags "$LDFLAGS" -v ./cmd/openvdc
 go build -ldflags "$LDFLAGS" -ldflags "-X 'github.com/axsh/openvdc/scheduler.ExecutorPath=${EXECUTOR_PATH}'" -v ./cmd/openvdc-scheduler
 go build -ldflags "$LDFLAGS" -v ./cmd/openvdc-executor
