ed0c06e648998475b5430a62f853efc80c2184f0
akirapop <cbaluta20@gmail.com>
2016-11-25 19:35:39 +0900

    Added debugging code.

--- deployment/docker/test-rpm-install.sh
+++ deployment/docker/test-rpm-install.sh
@@ -7,6 +7,12 @@ docker_rm() {
   if [[ -z "${CID}" ]]; then
     return 0
   fi
+  if [[ -n "$LEAVE_CONTAINER" ]]; then
+     if [[ "${LEAVE_CONTAINER}" != "0" ]]; then
+        echo "Skip to clean container: ${CID}"
+        return 0
+     fi
+  fi
   docker rm -f "${CID}"
 }
 trap 'docker_rm' EXIT
