6309d2f1dffd2368cbaf769b83d98279ffe1267a
Masahiro Fujiwara <m-fujiwara@axsh.net>
2016-11-27 12:57:45 +0900

    scheduler: Disable setting executor's absolute path at build time.

--- build.sh
+++ build.sh
@@ -7,12 +7,10 @@ SHA=$(git rev-parse --verify HEAD)
 BUILDDATE=$(date '+%Y/%m/%d %H:%M:%S %Z')
 GOVERSION=$(go version)
 LDFLAGS="-X 'main.version=${VERSION}' -X 'main.sha=${SHA}' -X 'main.builddate=${BUILDDATE}' -X 'main.goversion=${GOVERSION}'"
-# During development, assume that the executor binary locates in the build directory.
-EXECUTOR_PATH=$(pwd)/openvdc-executor
 
 $GOPATH/bin/govendor sync
 
 go build -ldflags "$LDFLAGS" -v ./cmd/openvdc
-go build -ldflags "$LDFLAGS" -ldflags "-X 'github.com/axsh/openvdc/scheduler.ExecutorPath=${EXECUTOR_PATH}'" -v ./cmd/openvdc-scheduler
+go build -ldflags "$LDFLAGS" -v ./cmd/openvdc-scheduler
 go build -ldflags "$LDFLAGS" -v ./cmd/openvdc-executor
 echo "Done"
