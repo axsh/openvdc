7e46581921707c8d0b8088d4e9faecddfdadf80c
Masahiro Fujiwara <m-fujiwara@axsh.net>
2016-11-30 14:03:53 +0900

    registry: Bundle JSON schema to binary.

--- build.sh
+++ build.sh
@@ -8,7 +8,14 @@ BUILDDATE=$(date '+%Y/%m/%d %H:%M:%S %Z')
 GOVERSION=$(go version)
 LDFLAGS="-X 'main.version=${VERSION}' -X 'main.sha=${SHA}' -X 'main.builddate=${BUILDDATE}' -X 'main.goversion=${GOVERSION}'"
 
+if [[ ! -x $GOPATH/bin/govendor ]]; then
+  go get -u github.com/kardianos/govendor
+fi
 $GOPATH/bin/govendor sync
+if [[ ! -x $GOPATH/bin/go-bindata ]]; then
+  go get -u github.com/jteeuwen/go-bindata/...
+fi
+$GOPATH/bin/go-bindata -pkg registry -o registry/schema.bindata.go schema
 
 go build -ldflags "$LDFLAGS" -v ./cmd/openvdc
 go build -ldflags "$LDFLAGS" -v ./cmd/openvdc-scheduler
