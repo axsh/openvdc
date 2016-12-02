1a703106f8ec022ed448c73c9578852105b36177
akirapop <cbaluta20@gmail.com>
2016-12-02 15:09:53 +0900

    Still not working. Reverting to old to check on something.

--- deployment/docker/unit-tests.sh
+++ deployment/docker/unit-tests.sh
@@ -43,5 +43,5 @@ docker cp . "${CID}:/var/tmp/go/src/github.com/axsh/openvdc"
 ## Run unit tests
 docker exec $CID /bin/bash -c "/usr/bin/env"
 docker exec $CID /bin/bash -c "cd /var/tmp/go/src/github.com/axsh/openvdc;  govendor sync"
-docker exec $CID /bin/bash -c "cd /var/tmp/go/src/github.com/axsh/openvdc;  ZK=127.0.0.1 go test '$(go list ./... | grep -v /vendor/)'"
-#docker exec $CID /bin/bash -c "cd /var/tmp/go/src/github.com/axsh/openvdc;  ZK=127.0.0.1 go test -v ./... "
+#docker exec $CID /bin/bash -c "cd /var/tmp/go/src/github.com/axsh/openvdc;  ZK=127.0.0.1 go test '$(go list ./... | grep -v /vendor/)'"
+docker exec $CID /bin/bash -c "cd /var/tmp/go/src/github.com/axsh/openvdc;  ZK=127.0.0.1 go test -v ./... "
