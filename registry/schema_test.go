e96b718f7c46065fe1c4ceecc414502f972c0897
Masahiro Fujiwara <m-fujiwara@axsh.net>
2016-12-05 14:55:35 +0900

    Refresh schema.bindata.

--- registry/schema_test.go
+++ registry/schema_test.go
@@ -13,7 +13,7 @@ const validJSON1 = `
   "$schema": "https://raw.githubusercontent.com/axsh/openvdc/master/schema/v1.json#",
   "title": "CentOS7",
   "template": {
-    "type": "lxc",
+    "type": "vm/lxc",
 		"lxc_image": {
 			"download_url": "https://images.linuxcontainers.org/1.0/images/d767cfe9a0df0b2213e28b39b61e8f79cb9b1e745eeed98c22bc5236f277309a/export"
 		}
