0a7c27f90fb752634dd831fd20f75f3025804386
toros11 <tobias.rosen@axsh.net>
2016-12-05 23:18:13 +0900

    Add step for mesosphere

---
+++ ci/multibox/ind-steps/step-mesosphere/init.sh
@@ -0,0 +1,8 @@
+
+
+(
+    $starting_step "Download mesosphere repo"
+    [[ -f "${TMP_ROOT}/etc/yum.repos.d/mesosphere.repo" ]]
+    $skip_step_if_already_done ; set -ex
+    sudo chroot ${TMP_ROOT} /bin/bash -c "yum install -y http://repos.mesosphere.io/el/7/noarch/RPMS/mesosphere-el-repo-7-1.noarch.rpm"
+) ; prev_cmd_failed
