bffe337a19702e121b956c577b5e7115ac0ddc74
b0r6 <tommy@axsh.net>
2016-12-05 17:22:10 +0900

    Added code to console-command

--- cmd/openvdc/cmd/console.go
+++ cmd/openvdc/cmd/console.go
@@ -3,6 +3,9 @@ package cmd
 import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"github.com/axsh/openvdc/api"
 )
 
 var consoleCmd = &cobra.Command{
@@ -11,7 +14,26 @@ var consoleCmd = &cobra.Command{
 	Long:  "Connect to an instance.",
 	RunE: func(cmd *cobra.Command, args []string) error {
 				
-		log.Infoln("Under construction")
-		return nil
+		if len(args) < 1 {
+                        log.Fatal("Please provide an instance ID")
+                }
+
+                instanceID := args[0]
+
+                req := &api.ConsoleRequest{
+                        InstanceId: instanceID,
+                }
+
+                return remoteCall(func(conn *grpc.ClientConn) error {
+                        c := api.NewInstanceClient(conn)
+
+                        res, err := c.Console(context.Background(), req)
+                        if err != nil {
+                                log.WithError(err).Fatal("Disconnected abnormally")
+                                return err
+                        }
+                        fmt.Println(res)
+                        return err
+                })
 	},
 }
