600dda7e494b5dcc9e0cce7666efd03e2199cf50
toros11 <tobias.rosen@axsh.net>
2016-12-06 19:01:56 +0900

    remove unnessecary flag

--- ci/multibox/ind-steps/step-openvdc/install.sh
+++ ci/multibox/ind-steps/step-openvdc/install.sh
@@ -2,5 +2,5 @@
     $starting_step "Install openvdc on ${vm_name}"
     [[ -f "${TMP_ROOT}/opt/axsh/openvdc/bin/openvdc" ]]
     $skip_step_if_already_done; set -ex
-    sudo chroot ${TMP_ROOT} /bin/bash -c "yum install -y openvdc --skip-broken"
+    sudo chroot ${TMP_ROOT} /bin/bash -c "yum install -y openvdc"
 ) ; prev_cmd_failed
