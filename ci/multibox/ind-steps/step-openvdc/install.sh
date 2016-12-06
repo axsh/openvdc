66260e9a42bd508f145d35dec7b3bcf08fddd63c
toros11 <tobias.rosen@axsh.net>
2016-12-06 19:01:07 +0900

    Check for openvdc install step

--- ci/multibox/ind-steps/step-openvdc/install.sh
+++ ci/multibox/ind-steps/step-openvdc/install.sh
@@ -1,7 +1,6 @@
 (
     $starting_step "Install openvdc on ${vm_name}"
-
-    false
+    [[ -f "${TMP_ROOT}/opt/axsh/openvdc/bin/openvdc" ]]
     $skip_step_if_already_done; set -ex
     sudo chroot ${TMP_ROOT} /bin/bash -c "yum install -y openvdc --skip-broken"
 ) ; prev_cmd_failed
