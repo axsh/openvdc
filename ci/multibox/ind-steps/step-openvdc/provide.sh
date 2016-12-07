0e89ae4f44ccea201e94d123dbd5ea4201722a20
toros11 <tobias.rosen@axsh.net>
2016-12-07 19:35:10 +0900

    Corrected some mistype variables

--- ci/multibox/ind-steps/step-openvdc/provide.sh
+++ ci/multibox/ind-steps/step-openvdc/provide.sh
@@ -2,7 +2,8 @@
 $scheduler && {
     (
         $starting_step "Starting scheduler on ${vm_name}"
+        false
         $skip_step_if_already_done ; set -ex
-        ssh root@${IP_ADDR} "/opt/axsh/openvdc/bin/openvdc-scheduler --master=${zk}:5050 --zk=${mesos_master}:2181 --api=${IP_ADDR}:5000"
+        ssh root@${IP_ADDR} "/opt/axsh/openvdc/bin/openvdc-scheduler --master=${mesos_master}:5050 --zk=${zk}:2181 --api=${IP_ADDR}:5000"
     ) ; prev_cmd_failed
 }
