273cec6c238068fed1a8b4f374dc2d9bcaf22fc8
Masahiro Fujiwara <m-fujiwara@axsh.net>
2016-12-12 15:55:16 +0900

    cli: Call MergeArgs() from “template validate” and “template show”.

--- cmd/openvdc/cmd/template/show.go
+++ cmd/openvdc/cmd/template/show.go
@@ -1,13 +1,18 @@
 package template
 
 import (
+	"fmt"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/axsh/openvdc/cmd/openvdc/internal/util"
+	"github.com/axsh/openvdc/handlers"
+	"github.com/axsh/openvdc/model"
+	"github.com/gogo/protobuf/proto"
 	"github.com/spf13/cobra"
 )
 
 var ShowCmd = &cobra.Command{
-	Use:   "show [Resource Template Path]",
+	Use:   "show [Resource Template Path] [template options]",
 	Short: "Show resource template details",
 	Long:  "Show resource template details",
 	Example: `
@@ -24,6 +29,22 @@ var ShowCmd = &cobra.Command{
 		if err != nil {
 			log.Fatal(err)
 		}
-		return rt.Template.ResourceHandler().ShowHelp(cmd.OutOrStdout())
+		clihn, ok := rt.Template.ResourceHandler().(handlers.CLIHandler)
+		if !ok {
+			return fmt.Errorf("%s does not support CLI interface", rt.Name)
+		}
+		pb := proto.Clone(rt.Template.Template.(proto.Message))
+		merged, ok := pb.(model.ResourceTemplate)
+		if !ok {
+			return fmt.Errorf("Failed to cast type")
+		}
+
+		err = clihn.MergeArgs(merged, args[1:])
+		if err != nil {
+			log.Fatalf("Failed to overwrite parameters for %s\n%s", rt.LocationURI(), err)
+		}
+
+		//TODO: Show difference between rt.ToModel() and merged objects.
+		return clihn.Usage(cmd.OutOrStdout())
 	},
 }
--- cmd/openvdc/cmd/template/validate.go
+++ cmd/openvdc/cmd/template/validate.go
@@ -1,13 +1,18 @@
 package template
 
 import (
+	"fmt"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/axsh/openvdc/cmd/openvdc/internal/util"
+	"github.com/axsh/openvdc/handlers"
+	"github.com/axsh/openvdc/model"
+	"github.com/gogo/protobuf/proto"
 	"github.com/spf13/cobra"
 )
 
 var ValidateCmd = &cobra.Command{
-	Use:     "validate [Resource Template Path]",
+	Use:     "validate [Resource Template Path] [template options]",
 	Aliases: []string{"test"},
 	Short:   "Validate resource template",
 	Long:    "Validate resource template",
@@ -16,14 +21,29 @@ var ValidateCmd = &cobra.Command{
 	% openvdc template validate ./templates/centos/7/null.json
 	% openvdc template validate https://raw.githubusercontent.com/axsh/openvdc/master/templates/centos/7/lxc.json
 	`,
+	DisableFlagParsing: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if len(args) < 1 {
 			return cmd.Usage()
 		}
 		templateSlug := args[0]
-		_, err := util.FetchTemplate(templateSlug)
+		rt, err := util.FetchTemplate(templateSlug)
 		if err != nil {
 			log.Fatal(err)
+		}
+		rh := rt.Template.ResourceHandler()
+		clihn, ok := rh.(handlers.CLIHandler)
+		if !ok {
+			return fmt.Errorf("%s does not support CLI interface", rt.Name)
+		}
+		pb := proto.Clone(rt.Template.Template.(proto.Message))
+		merged, ok := pb.(model.ResourceTemplate)
+		if !ok {
+			return fmt.Errorf("Failed to cast type")
+		}
+		err = clihn.MergeArgs(merged, args[1:])
+		if err != nil {
+			log.Fatalf("Failed to overwrite parameters for %s\n%s", rt.LocationURI(), err)
 		}
 		return nil
 	},
