90cd0bd2b1c8a647584e6687e01efbd1ad0b4a0d
toros11 <tobias.rosen@axsh.net>
2016-12-12 20:14:10 +0900

    Convert kill/destroy to symlinks

--- ci/multibox/10.0.100.12-vdc-scheduler/kill.sh
+++ ci/multibox/10.0.100.12-vdc-scheduler/kill.sh
@@ -1,9 +1,1 @@
-#!/bin/bash
-
-export ENV_ROOTDIR="$(cd "$(dirname $(readlink -f "$0"))/.." && pwd -P)"
-export NODE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-. "${NODE_DIR}/vmspec.conf"
-. "${ENV_ROOTDIR}/ind-steps/step-box/common.source"
-
-kill-vm
+../10.0.100.10-zookeeper/kill.sh
