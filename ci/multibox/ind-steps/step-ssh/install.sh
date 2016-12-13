7f221570864ad9146ef46fb4151c0f76deba72cc
toros11 <tobias.rosen@axsh.net>
2016-12-13 20:17:59 +0900

    Add options to set user

--- ci/multibox/ind-steps/step-ssh/install.sh
+++ ci/multibox/ind-steps/step-ssh/install.sh
@@ -1,31 +1,16 @@
 #!/bin/bash
 
 (
-    $starting_step "Create the public key and setup ssh config"
-    [[ -f ${NODE_DIR}/root@${vm_name} ]]
+    $starting_step "Create the public key and setup ssh config for ${user}"
+    [[ -f ${NODE_DIR}/${user}@${vm_name} ]]
     $skip_step_if_already_done; set -ex
-    ssh-keygen -t rsa -b 2048 -N "" -f ${NODE_DIR}/root@${vm_name}
-    chmod 600 ${NODE_DIR}/root@${vm_name}
-    chmod 600 ${NODE_DIR}/root@${vm_name}.pub
+    add_user_key "${user}"
 ) ; prev_cmd_failed
 
 (
-    $starting_step "Install authorized ssh key for ${vm_name}"
-    sudo bash -c "[ -f ${TMP_ROOT}/root/.ssh/authorized_keys ]"
-
+    $starting_step "Install authorized ssh key for ${user} on ${vm_name}"
+    sudo bash -c "[ -f ${TMP_ROOT}/${user}/.ssh/authorized_keys ]"
     $skip_step_if_already_done; set -ex
-    sudo chroot ${TMP_ROOT} /bin/bash -e <<EOS
-mkdir -p -m 600 /root/.ssh
-
-sed -i \
--e 's,^PermitRootLogin .*,PermitRootLogin yes,' \
--e 's,^PasswordAuthentication .*,PasswordAuthentication yes,' \
--e 's,^DenyUsers.root,#DenyUsers root,' \
-\
-/etc/ssh/sshd_config
-EOS
-    sudo cp "${NODE_DIR}/root@${vm_name}.pub" "${TMP_ROOT}/root/.ssh/authorized_keys"
-
+    install_user_key "${user}"
 ) ; prev_cmd_failed
-
 
