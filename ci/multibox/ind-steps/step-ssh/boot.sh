dded59436bedefc7c57c8a19c5b136d5a0e08030
toros11 <tobias.rosen@axsh.net>
2016-12-14 19:40:00 +0900

    Refactor ssh key handling

--- ci/multibox/ind-steps/step-ssh/boot.sh
+++ ci/multibox/ind-steps/step-ssh/boot.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 (
+    $starting_step "Import ssh key"
+    [ -f ${NODE_DIR}/sshkey ]
+    $skip_step_if_already_done
+    sudo cp ${cache_dir}/sshkey_${vm_name} ${NODE_DIR}/sshkey
+    sudo chown ${USER}:${USER} ${NODE_DIR}/sshkey
+) ; prev_cmd_failed
+
+(
     $starting_step "Wait for ssh"
     [[ "$(nc ${IP_ADDR} 22 < /dev/null)" == *"SSH"* ]]
     $skip_step_if_already_done ; set -xe
--- ci/multibox/ind-steps/step-ssh/install.sh
+++ ci/multibox/ind-steps/step-ssh/install.sh
@@ -2,15 +2,16 @@
 
 (
     $starting_step "Create the public key and setup ssh config for ${user}"
-    [[ -f ${NODE_DIR}/${user}@${vm_name} ]]
+    [[ -f ${cahe_dir}/sshkey_${vm_name} ]]
     $skip_step_if_already_done; set -ex
-    add_user_key "${user}"
+    add_user_key "${ci_user}"
+    sudo mv ${NODE_DIR}/sshkey ${cache_dir}/sshkey_${vm_name}
 ) ; prev_cmd_failed
+
 
 (
     $starting_step "Install authorized ssh key for ${user} on ${vm_name}"
     sudo bash -c "[ -f ${TMP_ROOT}/${user}/.ssh/authorized_keys ]"
     $skip_step_if_already_done; set -ex
-    install_user_key "${user}"
+    install_user_key "${ci_user}"
 ) ; prev_cmd_failed
-
