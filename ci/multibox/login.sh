a43b7c90b42766a70f56daabc94c5c63c106cf97
toros11 <tobias.rosen@axsh.net>
2016-12-14 19:40:58 +0900

    Update ssh key file name

--- ci/multibox/login.sh
+++ ci/multibox/login.sh
@@ -21,7 +21,7 @@ else
     }
 
     [[ ${PROTOCOL} == "ssh" ]] &&
-        ssh -i ${NODE_DIR}/root@${vm_name} root@${IP_ADDR}
+        ssh -i ${NODE_DIR}/sshkey root@${IP_ADDR}
     [[ ${PROTOCOL} == "telnet" ]] && {
         telnet_host="${serial%%,*}"
         telnet_host=${telnet_host#*:}
