a304ddf1ad1d9b360e84373b527bc32569b12bd9
toros11 <tobias.rosen@axsh.net>
2016-12-14 14:10:14 +0900

    Delete ssh key as part of destroy function

--- ci/multibox/10.0.100.10-zookeeper/destroy.sh
+++ ci/multibox/10.0.100.10-zookeeper/destroy.sh
@@ -5,7 +5,5 @@ export NODE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 . "${NODE_DIR}/vmspec.conf"
 . "${ENV_ROOTDIR}/ind-steps/step-box/common.source"
-. "${ENV_ROOTDIR}/ind-steps/step-ssh/common.source"
 
 destroy_vm
-rm ${NODE_DIR}/root@${vm_name}*
