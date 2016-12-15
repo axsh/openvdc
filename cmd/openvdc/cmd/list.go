eb28241a43fed0ce89c7d1d6344cc9ca2eec989d
Masahiro Fujiwara <m-fujiwara@axsh.net>
2016-12-15 22:15:01 +0900

    cli: Update "openvdc list" to call API.

--- cmd/openvdc/cmd/list.go
+++ cmd/openvdc/cmd/list.go
@@ -1,75 +1,37 @@
 package cmd
 
 import (
+	"fmt"
+
 	log "github.com/Sirupsen/logrus"
-	"github.com/spf13/cobra"
+	"github.com/axsh/openvdc/api"
+	"github.com/axsh/openvdc/cmd/openvdc/internal/util"
 	"github.com/axsh/openvdc/model"
+	"github.com/spf13/cobra"
 	"golang.org/x/net/context"
-	"os"
+	"google.golang.org/grpc"
 )
-
-func init() {
-	// TODO: Remove --server option from sub-command.
-	listCmd.PersistentFlags().StringVarP(&serverAddr, "server", "s", "localhost:5000", "gRPC API server address")
-	listCmd.PersistentFlags().SetAnnotation("server", cobra.BashCompSubdirsInDir, []string{})
-}
-
-var testZkServer string
-
-func getInstances(filter string, ctx context.Context) (string) {
-
-	var instanceFilter model.Instance_State
-
-	switch(filter) {
-		case "registered":
-			instanceFilter = model.Instance_REGISTERED
-
-		case "running":
-			instanceFilter = model.Instance_RUNNING
-	}
-
-	instances, err := model.Instances(ctx).FilterByState(instanceFilter)
-
-	if err != nil {
-		log.Errorln("Error: ", err)
-	}
-
-	var result string
-
-	if len(instances) > 0 {
-		for _, i := range instances {
-			result = result + " " + i.GetId()
-		}
-	} 
-		
-	return result
-}
-
 
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "List all registered/running instances",
-	Long:  "List all registered/running instances",
+	Short: "List instances",
+	Long:  "List instances",
 	RunE: func(cmd *cobra.Command, args []string) error {
-
-		if os.Getenv("ZK") != "" {
-                	testZkServer = os.Getenv("ZK")
-        	} else {
-                	testZkServer = "127.0.0.1"
+		req := &api.InstanceListRequest{
+			Filter: &api.InstanceListRequest_Filter{
+				State: model.InstanceState_REGISTERED,
+			},
 		}
-
-		ctx, err := model.Connect(context.Background(), []string{testZkServer})
-		if err != nil {
-			log.WithError(err).Error("Failed to connect to datasource:")
-		} else {
-			defer model.Close(ctx)
-		}
-
-		registeredInstances := getInstances("registered", ctx)
-		log.Infoln("Registered instances:\n" + registeredInstances)
-
-		runningInstances := getInstances("running", ctx)
-                log.Infoln("Running instances:\n" + runningInstances)
-
-		return nil
+		return util.RemoteCall(func(conn *grpc.ClientConn) error {
+			c := api.NewInstanceClient(conn)
+			res, err := c.List(context.Background(), req)
+			if err != nil {
+				log.WithError(err).Fatal("Disconnected abnormaly")
+				return err
+			}
+			for i, item := range res.Items {
+				fmt.Printf("%-5d %-10s %-10s\n", i, item.Id, item.State.String())
+			}
+			return err
+		})
 	}}
