620eb287ecd9051dfe1111fca9e0a6b4602e3a1d
toros11 <tobias.rosen@axsh.net>
2016-12-15 23:21:02 +0900

    Re-add kill option parameter

--- ci/multibox/destroy.sh
+++ ci/multibox/destroy.sh
@@ -6,6 +6,9 @@ export ENV_ROOTDIR="$(cd "$(dirname $(readlink -f "$0"))" && pwd -P)"
 copy_default_config
 . "${ENV_ROOTDIR}/config.source"
 
+kill_option=false
+[[ "$1" == "--kill" ]] && { kill_option=true ; shift ; }
+
 scheduled_nodes=${NODES[@]}
 [[ -n "$1" ]] && scheduled_nodes="${@}"
 
@@ -14,7 +17,7 @@ for node in ${scheduled_nodes[@]} ; do
         $starting_group "Destroy ${node%,*}"
         false
         $skip_group_if_unnecessary
-        ${ENV_ROOTDIR}/${node}/destroy.sh
+        ${kill_option} && { ${ENV_ROOTDIR}/${node}/kill.sh ; } || { ${ENV_ROOTDIR}/${node}/destroy.sh ; }
     ) ; prev_cmd_failed
 done
 
