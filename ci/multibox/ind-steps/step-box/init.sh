02f88349b59a188d288fc6df8a94857931fe83b5
toros11 <tobias.rosen@axsh.net>
2016-12-15 19:47:58 +0900

    Store template boxes with other CI related datas

--- ci/multibox/ind-steps/step-box/init.sh
+++ ci/multibox/ind-steps/step-box/init.sh
@@ -7,7 +7,7 @@ fi
     $starting_step "Deploy seed image for ${vm_name}"
     [ -f "$(vm_image)" ]
     $skip_step_if_already_done; set -xe
-    tar -Sxzf "${ENV_ROOTDIR}/boxes/${box}-${distr_ver}-${arch}.kvm.box" -C "${NODE_DIR}"
+    tar -Sxzf "/data/openvdc-ci/boxes/boxes/${box}-${distr_ver}-${arch}.kvm.box" -C "${NODE_DIR}"
     rm ${NODE_DIR}/box-disk1.rpm-qa
 ) ; prev_cmd_failed
 
