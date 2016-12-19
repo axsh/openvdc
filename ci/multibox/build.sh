990ab0b7c917554a1839914add62c1671e3c228a
toros11 <tobias.rosen@axsh.net>
2016-12-19 15:33:08 +0900

    Safer checking for cases when no base branch is set

--- ci/multibox/build.sh
+++ ci/multibox/build.sh
@@ -41,21 +41,21 @@ $REBUILD && {
             ) ; prev_cmd_failed
         done
     ) ; prev_cmd_failed
-
-    (
-        $starting_step "Create cache folder"
-            [ -d "${CACHE_DIR}/${BRANCH}" ]
-            $skip_step_if_already_done ; set -ex
-            mkdir -p "${CACHE_DIR}/${BRANCH}"
-    ) ; prev_cmd_failed
 } || {
     (
         $starting_step "Clone base images from ${BASE_BRANCH}"
-        [ -d "${CACHE_DIR}/${BRANCH}" ]
+        [ -d "${CACHE_DIR}/${BRANCH}" || ! -d "${CACHE_DIR}/${BASE_BRANCH}" ]
         $skip_step_if_already_done ; set -ex
         cp -r "${CACHE_DIR}/${BASE_BRANCH}" "${CACHE_DIR}/${BRANCH}"
     ) ; prev_cmd_failed
 }
+
+(
+    $starting_step "Create cache folder"
+    [ -d "${CACHE_DIR}/${BRANCH}" ]
+    $skip_step_if_already_done ; set -ex
+    mkdir -p "${CACHE_DIR}/${BRANCH}"
+) ; prev_cmd_failed
 
 masquerade "${NETWORK}/${PREFIX}"
 
