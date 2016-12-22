bebf5d65379cfb27e51fb896b430d9f71376a066
Masahiro Fujiwara <m-fujiwara@axsh.net>
2016-12-22 19:22:20 +0900

    build: Fix appveyor build.

--- build.ps1
+++ build.ps1
@@ -10,23 +10,28 @@ Invoke-Expression "$(Join-Path ${env:GOPATH}\bin govendor.exe -Resolve) sync"
 $modtime=$(git log -n 1 --date=raw --pretty=format:%cd -- schema/).split(" ", 2)[0]
 Invoke-Expression "$(Join-Path ${env:GOPATH}\bin go-bindata.exe -Resolve) -mode 420 -modtime ${modtime} -pkg registry -o registry\schema.bindata.go schema"
 
-# Determine the default branch reference for registry/github.go
-$SCHEMA_LAST_COMMIT=$(git log -n 1 --pretty=format:%H -- schema/ registry/schema.bindata.go)
-$f=$(git rev-list origin/master | Select-String "${SCHEMA_LAST_COMMIT}" -ErrorAction "Continue")
-if ( $f -ne $null ) {
-  # Found no changes for resource template/schema on HEAD.
-  # so that set preference to the master branch.
-  $LDFLAGS="${LDFLAGS} -X 'registry.GithubDefaultRef=master'"
-} else {
-  # Found resource template/schema changes on this HEAD. Switch the default reference branch.
-  # Check if $GIT_BRANCH has something once in case of running in Jenkins.
-  $branch = if( $env:GIT_BRANCH ){
-      $env:GIT_BRANCH
-  }else{
-      $(git rev-parse --abbrev-ref HEAD)
-  }
-  $LDFLAGS="${LDFLAGS} -X 'registry.GithubDefaultRef=${branch}'"
+$branch="master"
+if( $env:APPVEYOR_REPO_BRANCH ){
+    $branch = $env:APPVEYOR_REPO_BRANCH
+}else{
+    # Determine the default branch reference for registry/github.go
+    $SCHEMA_LAST_COMMIT=$(git log -n 1 --pretty=format:%H -- schema/ registry/schema.bindata.go)
+    $f=$(git rev-list origin/master | Select-String "${SCHEMA_LAST_COMMIT}" -ErrorAction "Continue")
+    $branch = if ( $f -ne $null ) {
+        # Found no changes for resource template/schema on HEAD.
+        # so that set preference to the master branch.
+        "master"
+    } else {
+        # Found resource template/schema changes on this HEAD. Switch the default reference branch.
+        # Check if $BRANCH_NAME has something once in case of running in Jenkins.
+        if( $env:BRANCH_NAME ){
+            $env:BRANCH_NAME
+        }else{
+            $(git rev-parse --abbrev-ref HEAD)
+        }
+    }
 }
+$LDFLAGS="${LDFLAGS} -X 'registry.GithubDefaultRef=${branch}'"
 
 go build -ldflags "$LDFLAGS" -v ./cmd/openvdc
 go build -ldflags "$LDFLAGS" -v ./cmd/openvdc-scheduler
