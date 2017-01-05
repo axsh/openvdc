b63c5f62d20f543e1ac313c98fb05e0b70c6dd06
Metallion <akieckens@axsh.net>
2017-01-05 17:18:43 +0900

    Added a step to generate the public key if it doesn't exist

--- ci/multibox/ind-steps/step-ssh/install.sh
+++ ci/multibox/ind-steps/step-ssh/install.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
+private_key="${CACHE_DIR}/${BRANCH}/sshkey_${vm_name}"
+public_key="${private_key}.pub"
+
 (
-    $starting_step "Create the public key and setup ssh config for ${user}"
+    $starting_step "Create the key pair and setup ssh config for ${user}"
     [[ -f ${CACHE_DIR}/${BRANCH}/sshkey_${vm_name} ]]
     $skip_step_if_already_done; set -ex
     add_user_key "${ci_user}"
-    mv ${NODE_DIR}/sshkey ${CACHE_DIR}/${BRANCH}/sshkey_${vm_name}
-    mv ${NODE_DIR}/sshkey.pub ${CACHE_DIR}/${BRANCH}/sshkey_${vm_name}.pub
+    mv ${NODE_DIR}/sshkey "${private_key}"
+    mv ${NODE_DIR}/sshkey.pub "${public_key}"
+) ; prev_cmd_failed
+
+(
+    # If the remains from older code are present, the private key will exist
+    # without the public key. In that case we need to generate it.
+    # Once enough time has passed for this to no longer occur, this step can
+    # be deleted.
+    $starting_step "Generate public key for ${user}"
+    [[ -f ${CACHE_DIR}/${BRANCH}/sshkey_${vm_name}.pub ]]
+    $skip_step_if_already_done; set -ex
+    ssh-keygen -y -f "${private_key}" > "${public_key}"
 ) ; prev_cmd_failed
 
 (
