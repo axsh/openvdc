d30e7d6d835b7953dfb93b6447021588b12a79aa
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-01-09 20:54:30 +0900

    cli: Setup .openvdc directory and the default config file.

--- cmd/openvdc/main.go
+++ cmd/openvdc/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 
@@ -9,33 +10,36 @@ import (
 	"github.com/axsh/openvdc/cmd/openvdc/internal/util"
 )
 
+const defaultTomlConfig = `
+[api]
+# endpoint = 127.0.0.1:5000
+[mesos]
+# address = 127.0.0.1:5050
+`
+
 func setupDefaultUserConfig(dir string) error {
 	stat, err := os.Stat(dir)
-	if os.IsExist(err) && !stat.IsDir() {
-		return fmt.Errorf("")
-	} else if os.IsNotExist(err) {
-		err = os.Mkdir(dir, 0755)
-		if err != nil {
-			return err
+	if err == nil {
+		if stat.IsDir() {
+			// nothing to do
+			return nil
+		} else {
+			return fmt.Errorf("%s is not directory", dir)
 		}
 	}
-	// Install default configuration file here.
-	confPath := filepath.Join(dir, "config")
-	_, err = os.Open(confPath)
-	if os.IsNotExist(err) {
-		f, err := os.Create(confPath)
-		if err != nil {
-			return err
-		}
-		defer f.Close()
+	err = os.Mkdir(dir, 0755)
+	if err != nil {
+		return err
 	}
-	return nil
+	confPath := filepath.Join(dir, "config.toml")
+	// Install default configuration file.
+	return ioutil.WriteFile(confPath, []byte(defaultTomlConfig), 0644)
 }
 
 func main() {
 	err := setupDefaultUserConfig(util.UserConfDir)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Failed to setup %s\n", util.UserConfDir)
+		fmt.Fprintf(os.Stderr, "Failed to setup %s: %v\n", util.UserConfDir, err)
 		os.Exit(1)
 	}
 	cmd.Execute()
