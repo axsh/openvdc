0b404191b7b6019979e4f5db8808977e0bf4db64
Metallion <akieckens@axsh.net>
2017-01-13 18:44:42 +0900

    Added a hack for jenkins to put the acceptance tests in GOPATH using a symlink so we can install dependencies

--- ci/tests/run_tests.sh
+++ ci/tests/run_tests.sh
@@ -2,6 +2,22 @@
 
 set -xe
 
+# Some temporary hacks to get openvdc in the GOPATH on the CI.
+# Will fix this once we start running the acceptance tests in docker containers.
+OPENVDC_ROOT="$(cd "$(dirname $(readlink -f "$0"))/../../" && pwd -P)"
+OPENVDC_LINKNAME="${GOPATH}/src/github.com/axsh/$(basename ${OPENVDC_ROOT})"
+
+function cleanup_symlink {
+  rm -f "${OPENVDC_LINKNAME}"
+}
+trap cleanup_symlink EXIT
+
+if [[ "${PWD##$GOPATH}" != "${PWD}"  ]]; then
+  mkdir -p "${GOPATH}/src/github.com/axsh/"
+  ln -s "${OPENVDC_ROOT}" "${OPENVDC_LINKNAME}"
+fi
+# End of temporary hacks
+
 if [[ ! -x $GOPATH/bin/govendor ]]; then
   go get -u github.com/kardianos/govendor
 fi
