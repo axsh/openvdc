6c193ecd4945843ea0a2b5887d2747415f245738
Metallion <akieckens@axsh.net>
2017-01-17 19:22:13 +0900

    Set the BRANCH, RELEASE_SUFFIX and REBUILD variables at build time

--- ci/acceptance-test/run_tests.sh
+++ ci/acceptance-test/run_tests.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+#TODO: Require a commandline option and output a usage is not supplied
+#      Just stopping people from running this locally without thinking. :p
 set -xe
+
+cat | sudo tee /etc/yum.repos.d/openvdc.repo << EOS
+[openvdc]
+name=OpenVDC
+failovermethod=priority
+baseurl=https://ci.openvdc.org/repos/${BRANCH}/${RELEASE_SUFFIX}
+enabled=1
+gpgcheck=0
+EOS
+
+sudo yum install -y openvdc-acceptance-test
 
 /multibox/build.sh
 /opt/axsh/openvdc/bin/openvdc-acceptance-test -test.v
