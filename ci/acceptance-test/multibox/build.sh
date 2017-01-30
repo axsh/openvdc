472b1aa6da3350178f4354cee9609545ff058f14
Metallion <akieckens@axsh.net>
2017-01-30 14:51:07 +0900

    Reuse the master branch cache when a branch is built for the first time and REBUILD isn't set

--- ci/acceptance-test/multibox/build.sh
+++ ci/acceptance-test/multibox/build.sh
@@ -58,8 +58,8 @@ if [[ "$REBUILD" == "true" ]]; then
     (
         $starting_group "Cleanup old environment"
         [ ! -d "${CACHE_DIR}/${BRANCH}" ]
-        $skip_group_if_unnecessary
-        rm -rf "${CACHE_DIR}/${BRANCH}"
+        $skip_group_if_unnecessary; set -x
+        rm -rf ${CACHE_DIR}/${BRANCH}/*
         for node in ${scheduled_nodes[@]} ; do
             (
                 $starting_group "Destroying ${node%,*}"
@@ -69,14 +69,15 @@ if [[ "$REBUILD" == "true" ]]; then
             ) ; prev_cmd_failed
         done
     ) ; prev_cmd_failed
+else
+    (
+        $starting_step "Copy cache from ${BASE_BRANCH} branch"
+        [ -d "${CACHE_DIR}/${BRANCH}" ]
+        $skip_step_if_already_done
+        mkdir -p "${CACHE_DIR}/${BRANCH}"
+        rsync -av "${CACHE_DIR}/${BASE_BRANCH}/" "${CACHE_DIR}/${BRANCH}/"
+    ) ; prev_cmd_failed
 fi
-
-(
-    $starting_step "Create cache folder"
-    [ -d "${CACHE_DIR}/${BRANCH}" ]
-    $skip_step_if_already_done ; set -ex
-    mkdir -p "${CACHE_DIR}/${BRANCH}"
-) ; prev_cmd_failed
 
 masquerade "${NETWORK}/${PREFIX}"
 
