99afb943f03dcd499254ffdee7625ce0ff739891
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-02-09 21:52:04 +0900

    Fix variable name.

--- ci/acceptance-test/tests/cmd_log_test.go
+++ ci/acceptance-test/tests/cmd_log_test.go
@@ -10,7 +10,7 @@ import (
 func TestOpenVDCLog(t *testing.T) {
 	stdout, _ := RunCmdAndReportFail(t, "openvdc", "run", "centos/7/lxc")
 	instance_id := strings.TrimSpace(stdout.String())
-	defer RunCmd("openvdc", "destroy", instanceID)
+	defer RunCmd("openvdc", "destroy", instance_id)
 
-	RunCmdAndReportFail(t, "openvdc", "log", instanceID)
+	RunCmdAndReportFail(t, "openvdc", "log", instance_id)
 }
