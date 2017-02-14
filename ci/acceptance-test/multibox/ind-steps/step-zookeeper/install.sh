b30f340a4e79702ab871b82c0eca358ad6b44c71
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-02-14 21:24:59 +0900

    ci: Fix daemon name

--- ci/acceptance-test/multibox/ind-steps/step-zookeeper/install.sh
+++ ci/acceptance-test/multibox/ind-steps/step-zookeeper/install.sh
@@ -3,5 +3,5 @@
     sudo chroot ${TMP_ROOT} /bin/bash -c "rpm -q mesos-zookeeper" > /dev/null
     $skip_step_if_already_done; set -ex
     sudo chroot ${TMP_ROOT} /bin/bash -c "yum install -y mesosphere-zookeeper"
-    $zk_host || sudo chroot ${TMP_ROOT} /bin/bash -c "systemctl disable mesosphere-zookeeper"
+    $zk_host || sudo chroot ${TMP_ROOT} /bin/bash -c "systemctl disable zookeeper"
 ) ; prev_cmd_failed
