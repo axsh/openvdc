acb7b3e0bf285c44f03fb86883de519ff19bf4aa
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-02-15 20:46:30 +0900

    ci: Wait instance become RUNNING.

--- ci/acceptance-test/tests/cmd_log_test.go
+++ ci/acceptance-test/tests/cmd_log_test.go
@@ -3,14 +3,32 @@
 package tests
 
 import (
+	"os/exec"
 	"strings"
 	"testing"
+	"time"
+
+	"github.com/tidwall/gjson"
 )
 
 func TestOpenVDCLog(t *testing.T) {
 	stdout, _ := RunCmdAndReportFail(t, "openvdc", "run", "centos/7/lxc")
 	instance_id := strings.TrimSpace(stdout.String())
 	defer RunCmd("openvdc", "destroy", instance_id)
+
+	waitUntil(t, 5 * time.Minute, func()error {
+		cmd := exec.Command("openvdc", "show", instance_id)
+		buf, err := cmd.CombinedOutput()
+		if err !=nil {
+			return err
+		}
+		result := gjson.GetBytes(buf, "instance.lastState.state")
+		if result.String() == "RUNNING" {
+			return nil
+		}
+		time.Sleep(5 * time.Second)
+		return WaitContinue
+	})
 
 	RunCmdAndReportFail(t, "openvdc", "log", instance_id)
 }
