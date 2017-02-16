f7cb2f344863a1b87324a26a55682e8283ceb076
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-02-16 20:39:21 +0900

    ci: Add reboot command test

---
+++ ci/acceptance-test/tests/cmd_reboot_test.go
@@ -0,0 +1,34 @@
+// +build acceptance
+
+package tests
+
+import (
+	"strings"
+	"testing"
+	"os/exec"
+	"time"
+
+	"github.com/tidwall/gjson"
+)
+
+func TestCmdReboot(t *testing.T) {
+	stdout, _ := RunCmdAndReportFail(t, "openvdc", "run", "centos/7/lxc")
+	instance_id := strings.TrimSpace(stdout.String())
+
+	waitUntil(t, 5 * time.Minute, func()error {
+		cmd := exec.Command("openvdc", "show", instance_id)
+		buf, err := cmd.CombinedOutput()
+		if err !=nil {
+			return err
+		}
+		result := gjson.GetBytes(buf, "instance.lastState.state")
+		if result.String() == "RUNNING" {
+			return nil
+		}
+		time.Sleep(5 * time.Second)
+		return WaitContinue
+	})
+
+	RunCmdAndReportFail(t, "openvdc", "reboot", instance_id)
+	RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
+}
