ef7ad692b830ae19f97e8c75b0bc2f33a0793549
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-02-16 13:11:08 +0900

    executor: Forward log messages to syslog.

--- cmd/openvdc-executor/init_linux.go
+++ cmd/openvdc-executor/init_linux.go
@@ -1,6 +1,20 @@
 package main
 
 import (
+	"log/syslog"
+
+	"github.com/Sirupsen/logrus"
+	logrus_syslog "github.com/Sirupsen/logrus/hooks/syslog"
+
 	_ "github.com/axsh/openvdc/hypervisor/lxc"
 	_ "github.com/axsh/openvdc/hypervisor/null"
 )
+
+func init() {
+	// forward log messages to local syslog.
+	hook, err := logrus_syslog.NewSyslogHook("", "", syslog.LOG_SYSLOG, "vdc-executor")
+	if err != nil {
+		logrus.Fatal("Failed to initialize syslog hook: ", err)
+	}
+	logrus.AddHook(hook)
+}
