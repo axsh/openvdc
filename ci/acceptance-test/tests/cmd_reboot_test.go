36bec148a1b18a636ad7b07c0f918984f616bb07
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-03-02 16:17:49 +0900

    ci/tests: Remove sudo in remote command

--- ci/acceptance-test/tests/cmd_reboot_test.go
+++ ci/acceptance-test/tests/cmd_reboot_test.go
@@ -17,7 +17,7 @@ func TestCmdReboot(t *testing.T) {
 	// TODO: For CentOS7, just needs to set 755 to the file. Then you'll see /var/lock/subsys/local with recent timestamp.
 	// RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, fmt.Sprintf("sudo lxc-attach -n %s -- chmod 755 /etc/rc.d/rc.local", instance_id), 10, 5)
 	// TODO: /etc/rc.local location is for ubuntu. Needs rewrite for CentOS7
-	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, fmt.Sprintf("sudo lxc-attach -n %s -- sh -c 'echo \"#!/bin/sh\ntouch /var/tmp/local\" >> /etc/rc.local'; sudo chmod 755 /etc/rc.local;", instance_id), 10, 5)
+	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, fmt.Sprintf("sudo lxc-attach -n %s -- sh -c 'echo \"#!/bin/sh\ntouch /var/tmp/local\" >> /etc/rc.local'; chmod 755 /etc/rc.local;", instance_id), 10, 5)
 	RunCmdAndReportFail(t, "openvdc", "reboot", instance_id)
 	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"REBOOTING"})
 	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, fmt.Sprintf("sudo lxc-attach -n %s -- test -f /var/tmp/local", instance_id), 10, 5)
