00d29f77d722f36b7a9b51f6ff20c18d405d9452
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-03-02 15:00:35 +0900

    ci/tests: reboot test: lxc-execute -> lxc-attach

--- ci/acceptance-test/tests/cmd_reboot_test.go
+++ ci/acceptance-test/tests/cmd_reboot_test.go
@@ -15,10 +15,10 @@ func TestCmdReboot(t *testing.T) {
 
 	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
 	// This test scenario is only valid for CentOS 7 /etc/rc.d/rc.local.
-	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, fmt.Sprintf("sudo lxc-execute -n %s -- chmod 755 /etc/rc.d/rc.local", instance_id), 10, 5)
+	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, fmt.Sprintf("sudo lxc-attach -n %s -- chmod 755 /etc/rc.d/rc.local", instance_id), 10, 5)
 	RunCmdAndReportFail(t, "openvdc", "reboot", instance_id)
 	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"REBOOTING"})
-	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, fmt.Sprintf("sudo lxc-execute -n %s -- test -f /var/lock/subsys/local", instance_id), 10, 5)
+	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, fmt.Sprintf("sudo lxc-attach -n %s -- test -f /var/lock/subsys/local", instance_id), 10, 5)
 	RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
 	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
 }
