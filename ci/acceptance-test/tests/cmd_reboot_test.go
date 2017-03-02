181397c48d6c83ccb90a7bdedf25d9f02e660960
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-03-03 08:35:46 +0900

    ci/tests: Enable rc-local.service for ubuntu and split test func for OS

--- ci/acceptance-test/tests/cmd_reboot_test.go
+++ ci/acceptance-test/tests/cmd_reboot_test.go
@@ -14,14 +14,25 @@ func TestCmdReboot(t *testing.T) {
 	instance_id := strings.TrimSpace(stdout.String())
 
 	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
-	// TODO: For CentOS7, just needs to set 755 to the file. Then you'll see /var/lock/subsys/local with recent timestamp.
-	// RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, fmt.Sprintf("sudo lxc-attach -n %s -- chmod 755 /etc/rc.d/rc.local", instance_id), 10, 5)
-	// TODO: /etc/rc.local location is for ubuntu. Needs rewrite for CentOS7
+
+	testCmdReboot_Ubuntu16(t, instance_id)
+	//testCmdReboot_Centos7(t, instance_id)
+
+	RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
+	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
+}
+
+func testCmdReboot_Ubuntu16(t *testing.T, instance_id string) {
+	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, fmt.Sprintf("sudo lxc-attach -n %s -- systemctl enable rc-local.service", instance_id), 10, 5)
 	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, fmt.Sprintf("sudo lxc-attach -n %s -- sh -c 'echo \"#!/bin/sh\\ntouch /var/local/openvdc\" > /etc/rc.local; chmod 755 /etc/rc.local;'", instance_id), 10, 5)
 	RunCmdAndReportFail(t, "openvdc", "reboot", instance_id)
 	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"REBOOTING"})
 	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, fmt.Sprintf("sudo lxc-attach -n %s -- test -f /var/local/openvdc", instance_id), 10, 5)
-	//RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, fmt.Sprintf("sudo lxc-attach -n %s -- test -f /var/lock/subsys/local", instance_id), 10, 5)
-	RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
-	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
+}
+
+func testCmdReboot_Centos7(t *testing.T, instance_id string) {
+	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, fmt.Sprintf("sudo lxc-attach -n %s -- chmod 755 /etc/rc.d/rc.local", instance_id), 10, 5)
+	RunCmdAndReportFail(t, "openvdc", "reboot", instance_id)
+	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"REBOOTING"})
+	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, fmt.Sprintf("sudo lxc-attach -n %s -- test -f /var/lock/subsys/local", instance_id), 10, 5)
 }
