2dd5693f70f03af54259d18fd704a42c0fffe119
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-03-03 12:17:16 +0900

    ci/tests: Wait for runlevel that can run rc scripts

--- ci/acceptance-test/tests/cmd_reboot_test.go
+++ ci/acceptance-test/tests/cmd_reboot_test.go
@@ -26,6 +26,17 @@ func testCmdReboot_Ubuntu14(t *testing.T, instance_id string) {
 	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, fmt.Sprintf("sudo lxc-attach -n %s -- sh -c 'echo \"#!/bin/sh\\ntouch /var/local/openvdc\\n\" > /etc/rc.local; chmod 755 /etc/rc.local; sync;'", instance_id), 10, 5)
 	RunCmdAndReportFail(t, "openvdc", "reboot", instance_id)
 	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"REBOOTING"})
+	WaitUntil(t, 6 * time.Minute, func() error {
+		stdout, _, err := RunSsh(executor_lxc_ip, fmt.Sprintf("sudo lxc-attach -n %s -- runlevel", instance_id))
+		if err != nil {
+			return err
+		}
+		if strings.Contains(stdout.String(), "unknown") {
+			time.Sleep(3 * time.Second)
+			return WaitContinue
+		}
+		return nil
+	})
 	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, fmt.Sprintf("sudo lxc-attach -n %s -- test -f /var/local/openvdc", instance_id), 10, 5)
 }
 
