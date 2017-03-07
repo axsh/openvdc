734911bdf7baafb24ab27dfd885faf9e6f43a6b4
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-03-07 11:33:27 +0900

    ci/tests: Case for console exec mode

--- ci/acceptance-test/tests/cmd_console_test.go
+++ ci/acceptance-test/tests/cmd_console_test.go
@@ -17,6 +17,9 @@ func TestCmdConsole_ShowOption(t *testing.T) {
 	RunCmdAndReportFail(t, "openvdc", "console", instance_id, "--show")
 	RunCmdAndReportFail(t, "sh", "-c", fmt.Sprintf("echo 'ls' | openvdc console %s", instance_id))
 	RunCmdAndExpectFail(t, "sh", "-c", fmt.Sprintf("echo 'false' | openvdc console %s", instance_id))
+	RunCmdAndReportFail(t, "sh", "-c", fmt.Sprintf("openvdc console %s ls", instance_id))
+	RunCmdAndReportFail(t, "sh", "-c", fmt.Sprintf("openvdc console %s -- ls", instance_id))
+	RunCmdAndExpectFail(t, "sh", "-c", fmt.Sprintf("openvdc console %s -- false", instance_id))
 	RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
 	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
 }
