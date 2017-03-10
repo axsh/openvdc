6e59da541966c0a37dcc49839f7661ae5c67a994
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-03-10 19:18:32 +0900

    ci/tests: Add scenario for "openvdc template"

---
+++ ci/acceptance-test/tests/cmd_template_test.go
@@ -0,0 +1,21 @@
+// +build acceptance
+
+package tests
+
+import (
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestCmdTemplateValidate(t *testing.T) {
+	RunCmdAndReportFail(t, "openvdc", "template", "validate", "centos/7/lxc")
+	RunCmdAndReportFail(t, "openvdc", "template", "validate", "./templates/centos/7/lxc.json")
+	RunCmdAndReportFail(t, "openvdc", "template", "validate", "https://raw.githubusercontent.com/axsh/openvdc/master/templates/centos/7/lxc.json")
+}
+
+func TestCmdTemplateShow(t *testing.T) {
+	RunCmdAndReportFail(t, "openvdc", "template", "show", "centos/7/lxc")
+	RunCmdAndReportFail(t, "openvdc", "template", "show", "./templates/centos/7/lxc.json")
+	RunCmdAndReportFail(t, "openvdc", "template", "show", "https://raw.githubusercontent.com/axsh/openvdc/master/templates/centos/7/lxc.json")
+}
