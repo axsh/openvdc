f006fa76e2e31f6beb845b8f56a4e0e1338e57cd
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-03-15 16:39:56 +0900

    cmd/wait: Gets --timeout option

--- cmd/openvdc/cmd/wait.go
+++ cmd/openvdc/cmd/wait.go
@@ -3,15 +3,21 @@ package cmd
 import (
 	"io"
 	"strings"
+	"time"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/axsh/openvdc/api"
 	"github.com/axsh/openvdc/cmd/openvdc/internal/util"
 	"github.com/axsh/openvdc/model"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
+
+func init() {
+	waitCmd.Flags().String("timeout", "", "Timeout to cancel wait.")
+}
 
 var waitCmd = &cobra.Command{
 	Use:   "wait [Instance ID] [Instance State]",
@@ -19,15 +25,31 @@ var waitCmd = &cobra.Command{
 	Long:  "Wait for the instance until the expected state",
 	Example: `
 	% openvdc wait i-0000000001 running
+	% openvdc wait i-0000000001 running --timeout=2h45m
+	% openvdc wait i-0000000001 running --timeout=10s
 	`,
+	SilenceUsage: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if len(args) <= 1 {
-			log.Fatalf("Missing instance ID and state")
+			cmd.Usage()
+			return errors.New("Missing instance ID and state")
 		}
 
 		instanceID := args[0]
 		goalStateExpected := args[1]
 
+		waitDuration := 999999 * time.Hour
+		if cmd.Flag("timeout").Changed {
+			v := cmd.Flag("timeout").Value.String()
+			var err error
+			waitDuration, err = time.ParseDuration(v)
+			if err != nil {
+				log.Fatalf("Invalid timeout value: %v", err)
+			}
+			if waitDuration < 0 {
+				log.Fatalf("Timeout value must be positive number")
+			}
+		}
 		goalState, ok := model.InstanceState_State_value[strings.ToUpper(goalStateExpected)]
 		if !ok {
 			log.Fatalf("Unknown instance state: %s", goalStateExpected)
@@ -59,20 +81,38 @@ var waitCmd = &cobra.Command{
 				log.WithError(err).Fatal("Failed Call /api.Instance/Event")
 				return err
 			}
-			for {
-				ev, err := stream.Recv()
-				if err != nil {
-					if err == io.EOF {
-						break
-					} else {
-						log.WithError(err).Fatal("Error streaming event")
+
+			quit := make(chan error, 1)
+
+			go func() {
+				defer close(quit)
+				for {
+					ev, err := stream.Recv()
+					if err != nil {
+						if err == io.EOF {
+							return
+						}
+						log.WithError(err).Error("Error streaming event")
+						quit <- err
+						return
+					}
+					if ev.GetState().State == model.InstanceState_State(goalState) {
+						quit <- nil
+						return
 					}
 				}
-				if ev.GetState().State == model.InstanceState_State(goalState) {
-					return nil
+			}()
+
+			tout := time.After(waitDuration)
+			for {
+				select {
+				case err := <-quit:
+					return err
+				case <-tout:
+					quit <- errors.New("Timedout")
 				}
 			}
-			return err
+			return nil
 		})
 	},
 }
