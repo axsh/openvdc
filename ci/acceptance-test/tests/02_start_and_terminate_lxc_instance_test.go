d46e02b4ea11aa8f2562137545d7a0299e89be52
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-03-15 16:45:53 +0900

    ci/tests: WaitInstance() -> openvdc wait

--- ci/acceptance-test/tests/02_start_and_terminate_lxc_instance_test.go
+++ ci/acceptance-test/tests/02_start_and_terminate_lxc_instance_test.go
@@ -13,11 +13,11 @@ func TestLXCInstance(t *testing.T) {
 	instance_id := strings.TrimSpace(stdout.String())
 
 	_, _ = RunCmdAndReportFail(t, "openvdc", "show", instance_id)
-	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
+	RunCmdAndReportFail(t, "openvdc", "wait", instance_id, "RUNNING")
 	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, "sudo lxc-info -n "+instance_id, 10, 5)
 	//TODO: Run only once after we've waited for RUNNING
 	_, _ = RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
-	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
+	RunCmdAndReportFail(t, "openvdc", "wait", instance_id, "TERMINATED")
 
 	//TODO: Don't rely on the standard 'command failed' error.
 	//It's more clear to say "container didn't get destroyed after calling openvdc destroy"
--- ci/acceptance-test/tests/cmd_log_test.go
+++ ci/acceptance-test/tests/cmd_log_test.go
@@ -13,6 +13,6 @@ func TestOpenVDCLog(t *testing.T) {
 	instance_id := strings.TrimSpace(stdout.String())
 	defer RunCmd("openvdc", "destroy", instance_id)
 
-	WaitInstance(t, 5 * time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
+	RunCmdAndReportFail(t, "openvdc", "wait", instance_id, "RUNNING")
 	RunCmdAndReportFail(t, "openvdc", "log", instance_id)
 }
