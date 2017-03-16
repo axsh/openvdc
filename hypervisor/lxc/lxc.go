ca9219cc3057a07e6a14fe30cf8a8c0006d74682
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-03-16 19:22:05 +0900

    lxc: Fix variable

--- hypervisor/lxc/lxc.go
+++ hypervisor/lxc/lxc.go
@@ -272,7 +272,7 @@ func (d *LXCHypervisorDriver) CreateInstance(i *model.Instance, in model.Resourc
 		return errors.Wrap(err, "Failed lxc.Create")
 	}
 
-	if err := d.modifyConf(lxcTmpl); err != nil {
+	if err := d.modifyConf(lxcResTmpl); err != nil {
 		return err
 	}
 
