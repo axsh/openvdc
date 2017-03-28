0bcd366a8b448464d941013d9b2d6cb6f25e78b8
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-03-28 12:24:52 +0900

    ci/tests: Add negative case test

--- ci/citest/acceptance-test/tests/cmd_template_test.go
+++ ci/citest/acceptance-test/tests/cmd_template_test.go
@@ -11,15 +11,19 @@ import (
 //go:generate go-bindata -pkg tests -o fixtures.bindata.go ./fixtures
 
 func init() {
-	if err := RestoreAsset("/var/tmp", "fixtures/lxc.json"); err != nil {
+	if err := RestoreAssets("/var/tmp", "fixtures"); err != nil {
 		panic(err)
 	}
 }
 
 func TestCmdTemplateValidate(t *testing.T) {
+	// Regular cases
 	RunCmdAndReportFail(t, "openvdc", "template", "validate", "centos/7/lxc")
 	RunCmdAndReportFail(t, "openvdc", "template", "validate", "/var/tmp/fixtures/lxc.json")
 	RunCmdAndReportFail(t, "openvdc", "template", "validate", "https://raw.githubusercontent.com/axsh/openvdc/master/templates/centos/7/lxc.json")
+
+	// Irregular cases
+	RunCmdAndExpectFail(t, "openvdc", "template", "validate", "/var/tmp/fixtures/invalid1.json")
 }
 
 func TestCmdTemplateShow(t *testing.T) {
