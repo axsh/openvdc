0cf7d89b98f75b2e49f9c93f6bd913527e7866af
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-04-03 22:19:23 +0900

    build: Use ZK 3.4.6 on appveyor

--- ci/appveyor/install-zk.ps1
+++ ci/appveyor/install-zk.ps1
@@ -2,10 +2,10 @@ $ErrorActionPreference = "Stop"
 
 Set-Location -Path $Args[0]
 
-Invoke-WebRequest -Uri http://www-us.apache.org/dist/zookeeper/stable/zookeeper-3.4.9.tar.gz -OutFile zookeeper.tar.gz
+Invoke-WebRequest -Uri http://www-us.apache.org/dist/zookeeper/zookeeper-3.4.6/zookeeper-3.4.6.tar.gz -OutFile zookeeper.tar.gz
 7z -y e "zookeeper.tar.gz"
 7z -y x "zookeeper.tar"
-Move-Item zookeeper-3.4.9\* .
+Move-Item zookeeper-3.4.6\* .
 New-Item data -type directory
 Move-Item conf\zoo_sample.cfg conf\zoo.cfg
 "dataDir=C:\ZooKeeper\data" | Out-File conf\zoo.cfg -Encoding ASCII -Append
