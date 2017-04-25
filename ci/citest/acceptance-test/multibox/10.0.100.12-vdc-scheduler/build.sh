a6e30a29501e562cbf4710ed22da6c7fc3b79358
b0r6 <b0r6@users.noreply.github.com>
2017-04-25 18:09:58 +0900

    Move code and copy image to scheduler box

--- ci/citest/acceptance-test/multibox/10.0.100.12-vdc-scheduler/build.sh
+++ ci/citest/acceptance-test/multibox/10.0.100.12-vdc-scheduler/build.sh
@@ -26,3 +26,50 @@ build "${IND_STEPS[@]}"
 install_openvdc_yum_repo
 install_yum_package_over_ssh "openvdc-scheduler"
 enable_service_over_ssh "openvdc-scheduler"
+
+function download_container_image () {
+        local img="${1}"
+        (
+                meta="meta.tar.xz"
+                rootfs="rootfs.tar.xz"
+
+                #Image not found, download it.
+                if [ ! -f "$IMG_DIR/$imgDirFolder/$meta" ]
+                then
+                        imgHost="https://uk.images.linuxcontainers.org"
+                        imgHostIndex="https://uk.images.linuxcontainers.org/meta/1.0/index-system"
+                        imgIndex=$(curl -s "$imgHostIndex")
+
+                        for item in ${imgIndex//\\n/}
+                        do
+                                if [[ $item =~ $img ]]
+                                then
+                                        IFS=';' read -a parts <<< "$item"
+
+                                        imageFolder=${parts[-1]}
+                                        imgHost+=$imageFolder
+                                        imgDirFolder=${img//";"/"-"}
+
+                                        mkdir -p "$IMG_DIR/$imgDirFolder"
+
+                                        printf "\nDownloading file:\n$imgHost$meta\n"
+                                        curl -o "$IMG_DIR/$imgDirFolder/$meta" "$imgHost/$meta"
+
+                                        printf "\nDownloading file:\n$imgHost$rootfs\n"
+                                        curl -o "$IMG_DIR/$imgDirFolder/$rootfs" "$imgHost/$rootfs"
+                                fi
+                        done
+                fi
+
+                #Copy image to scheduler box.
+		scp -i "${ENV_ROOTDIR}/10.0.100.12-vdc-scheduler/sshkey" "$imgHost/$meta" "root@10.0.100.12:/var/www/html/images/"
+
+
+        ) #; prev_cmd_failed. 
+}
+
+for img in "${IMAGES[@]}"
+do
+        download_container_image "${img}"
+done
+
