7cb8e410ff15da9ce2da3bb3600d6b86b52330e6
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-04-29 18:55:26 +0900

    hypervisor/null: Crash at the given stage.

--- hypervisor/null/null.go
+++ hypervisor/null/null.go
@@ -1,6 +1,8 @@
 package null
 
 import (
+	"fmt"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/axsh/openvdc/hypervisor"
 	"github.com/axsh/openvdc/model"
@@ -27,31 +29,47 @@ func (n *NullHypervisorProvider) CreateDriver(string) (hypervisor.HypervisorDriv
 }
 
 type NullHypervisorDriver struct {
-	log                    *log.Entry
+	log        *log.Entry
+	CrashStage model.NullTemplate_CrashStage
 }
 
 func (h *NullHypervisorDriver) StartInstance() error {
 	h.log.Infoln("StartInstance")
+	if h.CrashStage == model.NullTemplate_START {
+		return fmt.Errorf("Emulate crash at StartInstance")
+	}
 	return nil
 }
 
 func (h *NullHypervisorDriver) StopInstance() error {
 	h.log.Infoln("StopInstance")
+	if h.CrashStage == model.NullTemplate_STOP {
+		return fmt.Errorf("Emulate crash at StoptInstance")
+	}
 	return nil
 }
 
 func (h *NullHypervisorDriver) CreateInstance(*model.Instance, model.ResourceTemplate) error {
 	h.log.Infoln("CreateInstance")
+	if h.CrashStage == model.NullTemplate_CREATE {
+		return fmt.Errorf("Emulate crash at CreateInstance")
+	}
 	return nil
 }
 
 func (h *NullHypervisorDriver) DestroyInstance() error {
 	h.log.Infoln("DestroyInstance")
+	if h.CrashStage == model.NullTemplate_DESTROY {
+		return fmt.Errorf("Emulate crash at DestroyInstance")
+	}
 	return nil
 }
 
 func (h *NullHypervisorDriver) RebootInstance() error {
 	h.log.Infoln("RebootInstance")
+	if h.CrashStage == model.NullTemplate_REBOOT {
+		return fmt.Errorf("Emulate crash at RebootInstance")
+	}
 	return nil
 }
 
