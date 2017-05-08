774e5dd4974a16a5b55562d380d4563d91f19871
b0r6 <b0r6@users.noreply.github.com>
2017-05-08 17:01:35 +0900

    Added local-image test

---
+++ ci/citest/acceptance-test/tests/local_image_test.go
@@ -0,0 +1,27 @@
+// +build acceptance
+
+package tests
+
+import (
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestCrashRecovery(t *testing.T) {
+
+	// Use custom lxc-template.
+	stdout, _ := RunCmdAndReportFail(t, "openvdc", "run", "centos/7/lxc", `{"lxc_template":{"template":"openvdc"}}`)
+	instance_id := strings.TrimSpace(stdout.String())
+
+	_, _ = RunCmdAndReportFail(t, "openvdc", "show", instance_id)
+	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
+	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, "sudo lxc-info -n "+instance_id, 10, 5)
+
+	_, _ = RunCmdAndReportFail(t, "openvdc", "stop", instance_id)
+
+	WaitInstance(t, 5*time.Minute, instance_id, "STOPPED", []string{"RUNNING", "STOPPING"})
+
+	_, _ = RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
+	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
+}
