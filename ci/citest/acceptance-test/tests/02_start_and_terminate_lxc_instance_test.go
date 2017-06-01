5f6657c9e700010be563e52a2d590460c4a3b542
toros11 <tobias.rosen@axsh.net>
2017-06-01 17:52:09 +0900

    Add test case for two ovs nics

--- ci/citest/acceptance-test/tests/02_start_and_terminate_lxc_instance_test.go
+++ ci/citest/acceptance-test/tests/02_start_and_terminate_lxc_instance_test.go
@@ -25,7 +25,7 @@ func TestLXCInstance(t *testing.T) {
 	RunSshWithTimeoutAndExpectFail(t, executor_lxc_ip, "sudo lxc-info -n "+instance_id, 10, 5)
 }
 
-func TestLXCInstance_NICx2(t *testing.T) {
+func TestLXCInstance_LinuxBrNICx2(t *testing.T) {
 	stdout, _ := RunCmdAndReportFail(t, "openvdc", "run", "centos/7/lxc",
 		`{"interfaces":[{"type":"veth"}, {"type":"veth"}], "node_groups":["linuxbr"]}`)
 	instance_id := strings.TrimSpace(stdout.String())
@@ -55,7 +55,35 @@ func TestLXCInstance_NICx2(t *testing.T) {
 			t.Log("bridge link show dev "+instance_id+"_01: ", stdout.String())
 		}
 	}
+	RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
+	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
+}
 
+func TestLXCInstance_OvsNICx2(t *testing.T) {
+	stdout, _ := RunCmdAndReportFail(t, "openvdc", "run", "centos/7/lxc",
+		`{"interfaces":[{"type":"vif"}, {"type":"vif"}], "node_groups":["ovs"]}`)
+	instance_id := strings.TrimSpace(stdout.String())
+
+	RunCmdAndReportFail(t, "openvdc", "show", instance_id)
+	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
+	RunSshWithTimeoutAndReportFail(t, executor_lxc_ovs_ip, "sudo lxc-info -n "+instance_id, 10, 5)
+	stdout, _, err := RunSsh(executor_lxc_ovs_ip, fmt.Sprintf("sudo /usr/bin/ovs-vsctl port-to-br %s", instance_id+"_00"))
+	if err != nil {
+		t.Log(stdout)
+		t.Error(err)
+	} else {
+		if testing.Verbose() {
+			t.Log("ovs-vsctl port-to-br "+instance_id+"_00", stdout.String())
+		}
+	}
+	stdout, _, err = RunSsh(executor_lxc_ovs_ip, fmt.Sprintf("sudo /usr/bin/ovs-vsctl port-to-br %s", instance_id+"_01"))
+	if err != nil {
+		t.Error(stdout)
+	} else {
+		if testing.Verbose() {
+			t.Log("ovs-vsctl port-to-br "+instance_id+"_00", stdout.String())
+		}
+	}
 	RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
 	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
 }
