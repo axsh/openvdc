9047c983eda80997558b7d7ac17d20b976d88769
toros11 <tobias.rosen@axsh.net>
2017-06-05 18:37:04 +0900

    Refactor download images to bashsteps

---
+++ ci/citest/acceptance-test/multibox/ind-steps/step-vdc-images/init.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for img in "${IMAGES[@]}" ; do
+    item=$(grep "$img" <<< "$imgIndex")
+    IFS=';' read -a parts <<< "$item"
+    IFS=';' eval 'folders=($item)'
+    imgRemoteDir="${parts[-1]}"
+
+    prepare_container_image "${img}" "${folders[0]}/${folders[1]}/${folders[2]}" "$imgRemoteDir"
+    add_container_image "${img}" "${folders[0]}/${folders[1]}/${folders[2]}"
+done
+
+(
+    # resync again after images have been built
+    $starting_step "Synching guestroot for ${vm_name}"
+    # This step is set to false by default and we rely on rsyncs -u flag
+    # to take care of keeping the files updated
+    false
+    $skip_step_if_already_done; set -ex
+    sudo rsync -rv "${NODE_DIR}/guestroot/" "${TMP_ROOT}"
+) ; prev_cmd_failed
