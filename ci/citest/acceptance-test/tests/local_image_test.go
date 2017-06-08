7769392dc6272e98f6f469ec59e868c693797bbf
b0r6 <b0r6@users.noreply.github.com>
2017-06-08 19:03:50 +0900

    Got the test to work

--- ci/citest/acceptance-test/tests/local_image_test.go
+++ ci/citest/acceptance-test/tests/local_image_test.go
@@ -11,9 +11,8 @@ import (
 )
 
 func TestLocalImage(t *testing.T) {
-
 	// Use custom lxc-template.
-	stdout, _ := RunCmdAndReportFail(t, "openvdc", "run", "https://raw.githubusercontent.com/axsh/openvdc/template-misc/templates/centos/7/lxc2.json", `{"node_groups":["linuxbr"]}`)
+	stdout, _ := RunCmdAndReportFail(t, "openvdc", "run", "https://raw.githubusercontent.com/axsh/openvdc/template-misc/templates/centos/7/lxc2.json", `{"lxc_template":{"template":"openvdc"}, "node_groups":["linuxbr"]}`)
 	instance_id := strings.TrimSpace(stdout.String())
 
 	_, _ = RunCmdAndReportFail(t, "openvdc", "show", instance_id)
@@ -21,8 +20,8 @@ func TestLocalImage(t *testing.T) {
 	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, "sudo lxc-info -n "+instance_id, 10, 5)
 
 	configFile := filepath.Join("/var/lib/lxc/", instance_id, "config")
+	stdout, _, err := RunSsh(executor_lxc_ip, fmt.Sprintf("echo | sudo head -n 1 %s", configFile))
 
-	stdout, _, err := RunSsh(executor_lxc_ip, fmt.Sprintf("echo $(head -n 1 %s)", configFile))
 	if err != nil {
 		t.Error(err)
 	}
@@ -30,9 +29,11 @@ func TestLocalImage(t *testing.T) {
 		t.Errorf("Couldn't read %s", configFile)
 	}
 
-	s := strings.Split(stdout.String(), "/")
+	t.Log(stdout.String())
+
+	s := strings.Split(strings.TrimSpace(stdout.String()), "/")
 	templateUsed := s[len(s)-1]
-	if templateUsed != "" || templateUsed != "lxc-openvdc" {
+	if templateUsed == "" || templateUsed != "lxc-openvdc" {
 		t.Errorf("Expected templateUsed to be 'lxc-openvdc', got:  %s", templateUsed)
 	}
 
