67629e3dab9bc7e3bb76bd710226a316f61926ab
itouri <k-akiyama@axsh.net>
2017-06-09 18:39:05 +0900

    Adding CI test code for authentication parameters.

--- ci/citest/acceptance-test/tests/cmd_console_test.go
+++ ci/citest/acceptance-test/tests/cmd_console_test.go
@@ -23,3 +23,14 @@ func TestCmdConsole_ShowOption(t *testing.T) {
 	RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
 	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
 }
+
+func TestCmdConsole_Authentication(t *testing.T) {
+	stdout, _ := RunCmdAndReportFail(t, "openvdc", "run", "centos/7/lxc", `{"authentication_type":"none"}`)
+	instance_id := strings.TrimSpace(stdout.String())
+
+	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
+
+
+	RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
+	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
+}
