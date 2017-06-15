c961245583f6f9b2a116a3dac0eac4bc04b82590
toros11 <tobias.rosen@axsh.net>
2017-06-15 22:45:31 +0900

    Fix mistake in expected output

--- hypervisor/qemu/qemu_cmd_test.go
+++ hypervisor/qemu/qemu_cmd_test.go
@@ -37,7 +37,7 @@ func TestQemuBootCmd(t *testing.T) {
 		Display: "none",
 	})
 
-	assert.Equal(strings.Join(cmd, " "), "-smp 1 -m 512-enable-kvm -serial unix:serial,server,nowait -monitor unix:monitor,server,nowait -drive file=drive,format=raw,if=disk -netdev tap,ifname=if,id=if -device virtio-net-pci,netdev=if,mac=mac -display none")
+	assert.Equal(strings.Join(cmd, " "), "-smp 1 -m 512 -enable-kvm -serial unix:serial,server,nowait -monitor unix:monitor,server,nowait -drive file=drive,format=raw,if=disk -netdev tap,ifname=if,id=if -device virtio-net-pci,netdev=if,mac=mac -display none")
 
 }
 
