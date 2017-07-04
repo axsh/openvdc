7a61c4d7f9a12583a4a902291d3bbf1dba3e134c
toros11 <tobias.rosen@axsh.net>
2017-07-04 23:21:13 +0900

    Load viper config from main

--- cmd/qemu-ifup/main.go
+++ cmd/qemu-ifup/main.go
@@ -14,13 +14,13 @@ func runCmd(cmd string, args []string) {
 	c := exec.Command(cmd, args...)
 	if err := c.Run(); err != nil {
 		log.WithFields(log.Fields{
-			"cmd": cmd,
+			"cmd":  cmd,
 			"args": args,
 		}).Fatal("Failed to execute command")
 	}
 }
 
-func init() {
+func initConfig() error {
 	viper.SetDefault("bridges.type", "linux")
 	viper.SetDefault("bridges.name", "br0")
 	viper.SetConfigFile(DefaultConfPath)
@@ -30,13 +30,17 @@ func init() {
 	if err != nil {
 		if viper.ConfigFileUsed() == DefaultConfPath && os.IsNotExist(err) {
 			// Ignore default conf file does not exist.
-			return
+			return nil
 		}
-		log.WithError(err).Fatalf("Failed to load config %s", viper.ConfigFileUsed())
+		return err
 	}
+	return nil
 }
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.WithError(err).Fatalf("Failed to load config %s", viper.ConfigFileUsed())
+	}
 	config := viper.GetViper()
 	ifname := os.Args[1]
 
