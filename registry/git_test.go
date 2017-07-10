8613ecfe63b50304555eeb7f05096cb0646ff2dc
Masahiro Fujiwara <m-fujiwara@axsh.net>
2017-07-10 19:23:33 +0900

    registry: Add git registry unit tests

--- registry/git_test.go
+++ registry/git_test.go
@@ -26,3 +26,33 @@ func TestGitFetch(t *testing.T) {
 	err = reg.Fetch()
 	assert.NoError(err)
 }
+
+func TestGitFind(t *testing.T) {
+	assert := assert.New(t)
+	dir, err := ioutil.TempDir("", "reg-test")
+	assert.NoError(err, "Could not create temp dir for testing.")
+	defer func() {
+		os.RemoveAll(dir)
+	}()
+
+	reg := NewGitRegistry(dir, "https://github.com/axsh/openvdc.git")
+	reg.SubtreePath = "templates"
+	err = reg.Fetch()
+	assert.NoError(err)
+	// Try finding existing template name.
+	rt, err := reg.Find("centos/7/lxc")
+	assert.NoError(err)
+	assert.NotNil(rt)
+	assert.Equal(rt.source, reg)
+
+	// Try finding unknown template name.
+	rt, err = reg.Find("should-not-exist")
+	assert.Error(err)
+	assert.Nil(rt)
+
+	// Confirm to fail with unknown subtree path.
+	reg.SubtreePath = "nosuchdir"
+	rt, err = reg.Find("centos/7/lxc")
+	assert.Error(err)
+	assert.Nil(rt)
+}
