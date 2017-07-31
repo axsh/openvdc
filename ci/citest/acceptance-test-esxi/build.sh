7be5e82e20700d477b33a7cbfb544b25b92d2c38
b0r6 <tommy@axsh.net>
2017-07-31 18:05:50 +0900

    Add vars

--- ci/citest/acceptance-test-esxi/build.sh
+++ ci/citest/acceptance-test-esxi/build.sh
@@ -154,14 +154,15 @@ function setup_config () {
   set +x;
   ssh_cmd "cat > /etc/openvdc/executor.toml << EOS
 [hypervisor]
-esxi-url = '${GOVC_URL}'
+esxi-user = 'root'
+esxi-pass = '${ESXI_PASS}'
+esxi-ip = '${ESXI_IP}'
 esxi-datacenter = '${GOVC_DATACENTER}'
 esxi-insecure = ${GOVC_INSECURE}
 esxi-host-sshkey = '/etc/openvdc/esxi/id_rsa'
 esxi-vm-user = 'centos7'
 esxi-vm-pass = 'centos7'
 esxi-vm-datastore = '${VM_DATASTORE}'
-esxi-vm-network = '${NETWORK}'
 [executor-api]
 [console]
 [console.ssh]
