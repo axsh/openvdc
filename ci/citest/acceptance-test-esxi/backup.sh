045906d3d9b19f518a4fd0c8df431020ebddb58c
toros11 <tobias.rosen@axsh.net>
2017-08-07 20:54:26 +0900

    Create a symlink to the latest backup

--- ci/citest/acceptance-test-esxi/backup.sh
+++ ci/citest/acceptance-test-esxi/backup.sh
@@ -20,15 +20,16 @@ ssh_esxi "vim-cmd hostsvc/firmware/backup_config"
 backup_file="$(ssh_esxi find . \| grep configBundle-vmware.tgz)"
 scp_esxi "${backup_file#*.}" "${backup_file##*/}_$(date +%Y%m%d)"
 ssh_esxi "rm -r ${backup_file}"
-
+ln -s "${BACKUP_PATH}/${backup_file##*/}_$(date +%Y%m%d)" "${BACKUP_PATH}/configBundle-vmware.tgz_latest"
 # backup datastore
 
 # TODO: filter out the requried images/look into possible tools that will do this automatically
 datastore_volumes=( "$(ssh_esxi ls -la /vmfs/volumes \| grep datastore)" )
 for token in ${datastore_volumes[@]} ; do
     [[ "${next_id}" == "true" ]] && {
-        mkdir -p "${BACKUP_PATH}/${name}"
-        scp_esxi "/vmfs/volumes/${token}" "${name}"
+        mkdir -p "${BACKUP_PATH}/$(date +%Y%m%d)/${name}"
+        scp_esxi "/vmfs/volumes/${token}" "$(date +%Y%m%d)/${name}"
+        ln -s "${BACKUP_PATH}/$(date +%Y%m%d)/${name}" "${BACKUP_PATH}/latest"
 
         unset next_is_id
         unset name
