2a739bdcc05387e918b995f6715eed76f4422782
toros11 <tobias.rosen@axsh.net>
2017-08-08 19:41:36 +0900

    Track latest backup with symlink and allow to set which vms backup

--- ci/citest/acceptance-test-esxi/backup.sh
+++ ci/citest/acceptance-test-esxi/backup.sh
@@ -7,6 +7,15 @@
 : "${ESXI_KEY}:?"ESXI_KEY should be set"}" 
 : "${ESXI_USER:?"ESXI_USER should be set"}"
 
+BACKUP_PATH=$"${BACKUP_PATH}/$(date +%Y%m%d_%H%M%S)"
+BACKUP_OBJECTS=(
+    "datastore1/images"
+    "datastore2/base"
+    "datastore2/CentOS7"
+    "datastore2/images"
+    "datastore2/key"
+)
+
 ssh_esxi() {
     ssh -i "${ESXI_KEY}" "${ESXI_USER}@${ESXI_IP}" "${@}"
 }
@@ -15,22 +24,23 @@ scp_esxi() {
 }
 
 # backup config files
+mkdir -p "${BACKUP_PATH}"
 ssh_esxi "vim-cmd hostsvc/firmware/sync_config"
 ssh_esxi "vim-cmd hostsvc/firmware/backup_config"
 backup_file="$(ssh_esxi find . \| grep configBundle-vmware.tgz)"
-scp_esxi "${backup_file#*.}" "${backup_file##*/}_$(date +%Y%m%d)"
+scp_esxi "${backup_file#*.}" "${backup_file##*/}"
 ssh_esxi "rm -r ${backup_file}"
-ln -s "${BACKUP_PATH}/${backup_file##*/}_$(date +%Y%m%d)" "${BACKUP_PATH}/configBundle-vmware.tgz_latest"
+
 # backup datastore
 
 # TODO: filter out the requried images/look into possible tools that will do this automatically
 datastore_volumes=( "$(ssh_esxi ls -la /vmfs/volumes \| grep datastore)" )
-for token in ${datastore_volumes[@]} ; do
-    [[ "${next_id}" == "true" ]] && {
-        mkdir -p "${BACKUP_PATH}/$(date +%Y%m%d)/${name}"
-        scp_esxi "/vmfs/volumes/${token}" "$(date +%Y%m%d)/${name}"
-        ln -s "${BACKUP_PATH}/$(date +%Y%m%d)/${name}" "${BACKUP_PATH}/latest"
+datastore_id=()
 
+for token in ${datastore_volumes[@]} ; do
+    [[ "${next_is_id}" == "true" ]] && {
+        datastore_id+=( "${token}" )
+        mkdir -p "${BACKUP_PATH}/${name}/${token}"
         unset next_is_id
         unset name
         continue
@@ -38,3 +48,12 @@ for token in ${datastore_volumes[@]} ; do
     [[ "${token}" == *"->"* ]] && next_is_id=true
     [[ "${token}" == *"datastore"* ]] && name="${token}"
 done
+
+for bo in ${BACKUP_OBJECTS[@]} ; do
+    ds="${bo%/*}"
+    id="${ds#*datastore}"
+    # scp_esxi "/vmfs/volumes/${bo}" "${datastore_id[$(( id - 1 ))]}"
+done
+
+rm -f ${BACKUP_PATH%/*}/latest
+ln -s ${BACKUP_PATH} ${BACKUP_PATH%/*}/latest
