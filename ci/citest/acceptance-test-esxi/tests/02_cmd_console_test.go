78aeffe8b6d3c2ca081c7427293ee52c76bcc316
toros11 <tobias.rosen@axsh.net>
2017-09-14 22:55:15 +0900

    Remove sleep call

--- ci/citest/acceptance-test-esxi/tests/02_cmd_console_test.go
+++ ci/citest/acceptance-test-esxi/tests/02_cmd_console_test.go
@@ -5,22 +5,20 @@ package tests
 import (
 	"strings"
 	"testing"
-	"time"
 
 	tests "github.com/axsh/openvdc/ci/citest/acceptance-test/tests"
 )
 
 func TestEsxiCmdConsole_ShowOption(t *testing.T) {
-    stdout, _ := tests.RunCmdAndReportFail(t, "openvdc", "run", "centos/7/esxi")
-    instance_id := strings.TrimSpace(stdout.String())
-    tests.WaitInstance(t, 10*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
+	stdout, _ := tests.RunCmdAndReportFail(t, "openvdc", "run", "centos/7/esxi")
+	instance_id := strings.TrimSpace(stdout.String())
+	tests.WaitInstance(t, 10*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
 
-    time.Sleep(time.Second*3)
-    tests.RunCmdAndReportFail(t, "openvdc", "console", instance_id, "--show")
-    tests.RunCmdAndReportFail(t, "sh", "-c", strings.Join([]string{"openvdc", "console", instance_id, "ls"}, " "))
-    tests.RunCmdAndReportFail(t, "sh", "-c", strings.Join([]string{"openvdc", "console", instance_id, "--", "ls"}, " "))
-    tests.RunCmdAndExpectFail(t, "sh", "-c", strings.Join([]string{"openvdc", "console", instance_id, "--", "false"}, " "))
+	tests.RunCmdAndReportFail(t, "openvdc", "console", instance_id, "--show")
+	tests.RunCmdAndReportFail(t, "sh", "-c", strings.Join([]string{"openvdc", "console", instance_id, "ls"}, " "))
+	tests.RunCmdAndReportFail(t, "sh", "-c", strings.Join([]string{"openvdc", "console", instance_id, "--", "ls"}, " "))
+	tests.RunCmdAndExpectFail(t, "sh", "-c", strings.Join([]string{"openvdc", "console", instance_id, "--", "false"}, " "))
 
-    tests.RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
-    tests.WaitInstance(t, 10*time.Minute, instance_id, "TERMINATED", nil)
+	tests.RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
+	tests.WaitInstance(t, 10*time.Minute, instance_id, "TERMINATED", nil)
 }
