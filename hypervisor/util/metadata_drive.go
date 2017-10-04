7f2df4e3777012c368984fa879372092de6ba7c5
toros11 <tobias.rosen@axsh.net>
2017-10-04 16:38:31 +0900

    Improve error handling

--- hypervisor/util/metadata_drive.go
+++ hypervisor/util/metadata_drive.go
@@ -17,8 +17,9 @@ type MetadataDrive interface {
 
 func runCmd(cmd string, args []string) error {
 	c := exec.Command(cmd, args...)
+
 	if err := c.Run(); err != nil {
-		return errors.Errorf("failed to execute command :%s %s", cmd, args)
+		return errors.Wrapf(err, "failed to execute command :%s %s", cmd, args)
 	}
 	return nil
 }
@@ -26,12 +27,13 @@ func runCmd(cmd string, args []string) error {
 func renderData(keyPath string, key string, value interface{}) error {
 	switch value.(type) {
 	case string:
-		ioutil.WriteFile(filepath.Join(keyPath, key), []byte(value.(string)), 0644)
-		return nil
+		if err := ioutil.WriteFile(filepath.Join(keyPath, key), []byte(value.(string)), 0644); err != nil {
+			return errors.Wrapf(err, "failed to write to file: %s", filepath.Join(keyPath, key))
+		}
 	default:
 		kp := filepath.Join(keyPath, key)
 		if err := os.MkdirAll(kp, os.ModePerm); err != nil {
-			return errors.Errorf("Unable to create folder: %s", kp)
+			return errors.Wrapf(err, "Unable to create folder: %s", kp)
 		}
 		for key, value := range value.(map[string]interface{}) {
 			if err := renderData(kp, key, value); err != nil {
@@ -39,7 +41,6 @@ func renderData(keyPath string, key string, value interface{}) error {
 			}
 		}
 	}
-
 	return nil
 }
 
@@ -47,23 +48,21 @@ func WriteMetadata(md MetadataDrive) error {
 	mountPath := filepath.Join(filepath.Dir(md.MetadataDrivePath()), "meta-data")
 
 	if err := os.MkdirAll(mountPath, os.ModePerm); err != nil {
-		return errors.Errorf("Unable to create folder: %s", mountPath)
+		return errors.Wrapf(err, "failed to create folder: %s", mountPath)
 	}
 	if err := runCmd("mount", []string{md.MetadataDrivePath(), mountPath}); err != nil {
-		return errors.Errorf("Error: %s", err)
+		return errors.Wrap(err, "failed to mount metadrive image")
 	}
-
 	for key, value := range md.MetadataDriveDatamap() {
 		if err := renderData(mountPath, key, value); err != nil {
 			return err
 		}
 	}
-
 	if err := runCmd("umount", []string{mountPath}); err != nil {
-		return errors.Errorf("Error: %s", err)
+		return errors.Wrap(err, "failed to umount metadrive image")
 	}
 	if err := os.RemoveAll(mountPath); err != nil {
-		return errors.Errorf("Unable remove path: %s", mountPath)
+		return errors.Wrapf(err, "falied to remove folder: %s", mountPath)
 	}
 	return nil
 }
@@ -73,14 +72,13 @@ func CreateMetadataDisk(md MetadataDrive) error {
 
 	if _, err := os.Stat(md.MetadataDrivePath()); err != nil {
 		if err := runCmd("mkfs.msdos", []string{"-C", "-s", "1", md.MetadataDrivePath(), "1440"}); err != nil {
-			return errors.Errorf("Error: %s", err)
+			return errors.Wrap(err, "failed to create metadrive image")
 		}
 	} else {
 		if err := runCmd("mkfs.msdos", []string{"-s", "1", md.MetadataDrivePath()}); err != nil {
-			return errors.Errorf("Error: %s", err)
+			return errors.Wrap(err, "failed to format metadrive image")
 		}
 	}
-
 	return nil
 }
 
