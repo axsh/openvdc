d35b3809734c686b9c4d329244a33e8e5235d681
b0r6 <tommy@axsh.net>
2017-10-06 14:09:49 +0900

    Increase sleep to 30 sec to see if it makes any difference

--- ci/citest/acceptance-test/tests/crash_recovery_test.go
+++ ci/citest/acceptance-test/tests/crash_recovery_test.go
@@ -29,7 +29,7 @@ func TestCrashRecovery(t *testing.T) {
 	t.Log("Waiting for instance "+instance_id+" to recover...")
 	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
 
-	time.Sleep(10 * time.Second)
+	time.Sleep(30 * time.Second)
 	t.Log("Attempting to destroy instance "+instance_id+"...")
 	_, _ = RunCmdAndReportFail(t, "openvdc", "destroy", instance_id)
 
