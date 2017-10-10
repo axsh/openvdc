0e8d99fd69ba4314635132aa7f655b77baa67306
b0r6 <b0r6@users.noreply.github.com>
2017-10-10 19:14:24 +0900

    Tweak test

--- ci/citest/acceptance-test/tests/crash_recovery_test.go
+++ ci/citest/acceptance-test/tests/crash_recovery_test.go
@@ -15,6 +15,10 @@ func TestCrashRecovery(t *testing.T) {
 	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
 	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, "sudo lxc-info -n "+instance_id, 10, 5)
 
+	_, _ = RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "stop", instance_id)
+	t.Log("Waiting for instance to become STOPPED...")
+	WaitInstance(t, 5*time.Minute, instance_id, "STOPPED", []string{"RUNNING", "STOPPING"})
+
 	//Simulate crash
 	t.Log("Simulate crash by stopping mesos-slave...")
 	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, "sudo systemctl stop mesos-slave", 10, 5)
@@ -26,12 +30,13 @@ func TestCrashRecovery(t *testing.T) {
 	t.Log("Re-start mesos-slave...")
 	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, "sudo systemctl start mesos-slave", 10, 5)
 
-	t.Log("Waiting for instance "+instance_id+" to recover...")
-	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
-
 	time.Sleep(30 * time.Second)
-	t.Log("Attempting to destroy instance "+instance_id+"...")
-	_, _ = RunCmdAndReportFail(t, "openvdc", "destroy", instance_id)
+
+	_, _ = RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "start", instance_id)
+	t.Log("Waiting for instance to become RUNNING...")
+	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"STOPPED", "STARTING"})
+
+	_, _ = RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
 
 	t.Log("Waiting for instance "+instance_id+" to become TERMINATED...")
 	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
