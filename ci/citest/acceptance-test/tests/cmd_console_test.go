853ffa6b13d83670d983447a30e3624d9d3aa246
itouri <k-akiyama@axsh.net>
2017-10-13 19:38:39 +0900

    Modify the test of console with public key.

--- ci/citest/acceptance-test/tests/cmd_console_test.go
+++ ci/citest/acceptance-test/tests/cmd_console_test.go
@@ -22,11 +22,12 @@ func runConsoleCmd(instance_id string, t *testing.T) {
 	RunCmdAndExpectFail(t, "sh", "-c", fmt.Sprintf("openvdc console %s -- false", instance_id))
 }
 
-func runConsoleCmdWithPrivatekey(instance_id string, private_key_path string, t *testing.T) {
-	RunCmdAndReportFail(t, "openvdc", "console", instance_id, "-i", private_key_path)
-	RunCmdAndReportFail(t, "sh", "-c", fmt.Sprintf("openvdc console %s -i %s ls", instance_id, private_key_path))
-	RunCmdAndReportFail(t, "sh", "-c", fmt.Sprintf("openvdc console %s -i %s -- ls", instance_id, private_key_path))
-	RunCmdAndExpectFail(t, "sh", "-c", fmt.Sprintf("openvdc console %s -i %s -- false", instance_id, private_key_path))
+func runConsoleCmdWithPrivatekey(instance_id string, private_key_path string, t *testing.T, expect_fail bool) {
+	if expect_fail {
+		RunCmdAndExpectFail(t, "openvdc", "console", instance_id, "-i", private_key_path)
+	} else {
+		RunCmdAndReportFail(t, "openvdc", "console", instance_id, "-i", private_key_path)
+	}
 }
 
 func TestCmdConsole_ShowOptionAuthenticationNone(t *testing.T) {
@@ -57,7 +58,6 @@ func TestLXCCmdConsole_AuthenticationPubkey(t *testing.T) {
 	if err != nil {
 		t.Fatalf("err: %s", err)
 	}
-
 	_, _, err = RunCmd("ssh-keygen", "-t", "rsa", "-f", private_key_path2, "-C", "", "-N", "")
 	if err != nil {
 		t.Fatalf("err: %s", err)
@@ -74,8 +74,8 @@ func TestLXCCmdConsole_AuthenticationPubkey(t *testing.T) {
 	// runConsole()
 	instance_id := strings.TrimSpace(stdout.String())
 	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
-	runConsoleCmdWithPrivatekey(instance_id, private_key_path, t)
-	runConsoleCmdWithPrivatekey(instance_id, private_key_path2, t) // This can not be authenticated.
+	runConsoleCmdWithPrivatekey(instance_id, private_key_path, t, false)
+	runConsoleCmdWithPrivatekey(instance_id, private_key_path2, t, true) // This can not be authenticated.
 	//vrunConsoleCmdPiped(instance_id, t)
 	RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
 	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
