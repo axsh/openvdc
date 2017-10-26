3535d652934e29cbdecb00bd2e324fa861c0fffe
toros11 <tobias.rosen@axsh.net>
2017-10-26 20:15:29 +0900

    Update tests

--- resource/collector/esxi/esxi_collector_test.go
+++ resource/collector/esxi/esxi_collector_test.go
@@ -2,10 +2,18 @@ package esxi
 
 import (
 	"testing"
+	"github.com/spf13/viper"
 )
 
 func TestNewEsxiResourceCollector(t *testing.T) {
-	c, err := NewEsxiResourceCollector() 
+	viper.SetDefault("resource-collector.mode", "esxi")
+	viper.SetDefault("hypervisor.esxi-user", "test")
+	viper.SetDefault("hypervisor.esxi-pass", "test")
+	viper.SetDefault("hypervisor.esxi-ip", "0.0.0.0")
+	viper.SetDefault("hypervisor.esxi-datacenter", "test")
+	viper.SetDefault("hypervisor.esxi-datastore", "test")
+
+	c, err := NewEsxiResourceCollector(viper.GetViper()) 
 	if c == nil && err != nil {
 		t.Errorf("Unable to create esxi resource collector.")
 	}
--- resource/collector/local/local_collector_test.go
+++ resource/collector/local/local_collector_test.go
@@ -2,10 +2,12 @@ package local
 
 import (
 	"testing"
+	"github.com/spf13/viper"
 )
 
 func TestNewLocalResourceCollector(t *testing.T) {
-	c, err := NewLocalResourceCollector()
+	viper.SetDefault("resource-collector.mode", "local")
+	c, err := NewLocalResourceCollector(viper.GetViper())
 	if c == nil && err != nil {
 		t.Errorf("Unable to create local resource collector.")
 	}
