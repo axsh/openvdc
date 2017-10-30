7116e9601f08eb30c4bdde25c290667fd14c4cfd
Metallion <akieckens@axsh.net>
2017-10-30 18:23:20 +0900

    Added a comment explaining why we're not using the standard epel-release

--- ci/citest/acceptance-test/multibox/ind-steps/step-epel/pre_boot.sh
+++ ci/citest/acceptance-test/multibox/ind-steps/step-epel/pre_boot.sh
@@ -6,6 +6,7 @@
         "rpm -qa epel-release* | egrep -q epel-release"
     [[ $? -eq 0 ]]
     $skip_step_if_already_done; set -xe
+    # epel-release.rpm from CentOS/extra contains deprecated index for mirror sites.
     sudo chroot "${TMP_ROOT}" /bin/bash -e <<'EOF'
 rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/Packages/e/epel-release-7-11.noarch.rpm
 EOF
