c32bd4d62cff26f6097f9ca09fcfb2dbbbf625ab
b0r6 <b0r6@users.noreply.github.com>
2017-11-07 17:42:59 +0900

    Some rearrangement

--- api/instance_service.go
+++ api/instance_service.go
@@ -21,9 +21,28 @@ type InstanceAPI struct {
 	api *APIServer
 }
 
-func (s *InstanceAPI) Copy(ctx context.Context, in *CopyRequest) (*CopyReply, error) {
+func (s *InstanceAPI) Copy(ctx context.Context, in *CopyRequest) (*CopyReply, error) {	
+	instanceID := in.InstanceId
+        if instanceID == "" {
+                return nil, fmt.Errorf("Invalid Instance ID")
+        }
 
-        return &CopyReply{InstanceId: ""}, nil
+        inst, err := model.Instances(ctx).FindByID(in.GetInstanceId())
+        if err != nil {
+                log.WithError(err).WithField("instance_id", in.GetInstanceId()).Error("Failed to find the instance")
+                return nil, err
+        }
+
+        node := &model.ExecutorNode{}
+        if err := model.Cluster(ctx).Find(inst.GetSlaveId(), node); err != nil {
+                log.WithError(err).WithField("instance_id", in.GetInstanceId()).Error("Failed to find the instance")
+                return nil, err
+        }
+
+        return &CopyReply{
+                InstanceId: instanceID,
+                Address:    node.Console.BindAddr,
+        }, nil
 }
 
 func (s *InstanceAPI) Create(ctx context.Context, in *CreateRequest) (*CreateReply, error) {
--- cmd/openvdc/cmd/copy.go
+++ cmd/openvdc/cmd/copy.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"strings"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/axsh/openvdc/api"
@@ -26,12 +27,24 @@ var copyCmd = &cobra.Command{
 			log.Fatalf("Please provide a destination path.")
 		}
 
-		src := args[0]
+		//src := args[0]
 		dest := args[1]
 
+		path := strings.Split(dest, ":")
+        	fmt.Sprintf("value: %s", path[0])
+
+        	instanceID, instanceDir := path[0], path[1]
+
+        	if instanceID == "" {
+               		log.Fatalf("Invalid Instance ID")
+        	}
+
+        	if instanceDir == "" {
+                	log.Fatalf("Invalid destination path")
+        	}
+
 		req := &api.CopyRequest{
-			Src: src,
-			Dest: dest,
+			InstanceId: instanceID,
 		}
 
 		return util.RemoteCall(func(conn *grpc.ClientConn) error {
@@ -41,7 +54,8 @@ var copyCmd = &cobra.Command{
 				log.WithError(err).Fatal("Disconnected abnormaly")
 				return err
 			}
-			fmt.Println(res)
+
+			fmt.Println(res.GetAddress())
 			return err
 		})
 	},
