fd91a4e5fd1b09a97f36705cfa23eb64183abe8e
toros11 <tobias.rosen@axsh.net>
2017-11-08 23:15:57 +0900

    Implement ConsoleWaitError

--- hypervisor/util/serial_connection.go
+++ hypervisor/util/serial_connection.go
@@ -6,6 +6,7 @@ import (
 	"net"
 	"os"
 	"sync"
+	"fmt"
 	"time"
 
 	log "github.com/Sirupsen/logrus"
@@ -15,6 +16,7 @@ import (
 
 type SerialConnection struct {
 	SerialConn net.Conn
+	err        error
 }
 
 type serialConsoleParam struct {
@@ -25,11 +27,10 @@ type serialConsoleParam struct {
 }
 
 func (sc *SerialConnection) stdinToConn(param *serialConsoleParam, finished <-chan struct{}) {
-	var err error
 	param.waitClosed.Add(1)
 	defer func() {
 		param.closeChan <-true
-		param.errc <-err
+		param.errc <-sc
 		param.waitClosed.Done()
 	}()
 
@@ -46,6 +47,7 @@ func (sc *SerialConnection) stdinToConn(param *serialConsoleParam, finished <-ch
 				} else {
 					log.WithError(err).Error("Failed to read from the from the console input buffer")
 				}
+				sc.err = err
 				return
 			}
 
@@ -54,9 +56,9 @@ func (sc *SerialConnection) stdinToConn(param *serialConsoleParam, finished <-ch
 				return
 			}
 
-			_, err = sc.SerialConn.Write(b[0:n])
-			if err != nil {
+			if _, err := sc.SerialConn.Write(b[0:n]); err != nil {
 				log.WithError(err).Error("Failed to write to the connection from the buffer")
+				sc.err = err
 				return
 			}
 		}
@@ -64,19 +66,10 @@ func (sc *SerialConnection) stdinToConn(param *serialConsoleParam, finished <-ch
 }
 
 func (sc *SerialConnection) connToStdout(param *serialConsoleParam, finished <-chan struct{}) {
-	var err error
 	param.waitClosed.Add(1)
 	defer func() {
-		if _, err = param.remoteConsole.Stdout.Write([]byte{0x0A}); err != nil {
-			if err == io.EOF {
-				err = nil
-			} else {
-				log.WithError(err).Error("Failed to write the linefeed character on exit")
-			}
-		}
-
 		param.closeChan <-true
-		param.errc <-err
+		param.errc <-sc
 		param.waitClosed.Done()
 	}()
 
@@ -84,41 +77,49 @@ func (sc *SerialConnection) connToStdout(param *serialConsoleParam, finished <-c
 	for {
 		sc.SerialConn.SetDeadline(time.Now().Add(time.Second))
 		n, err := sc.SerialConn.Read(b)
+		if err == io.EOF {
+			sc.err = errors.New("connection lost")
+			return
+		}
 
 		select {
 		case <-finished:
 			return
 		default:
-			if err == io.EOF {
-				_, err = param.remoteConsole.Stdout.Write([]byte("\nConnection lost\n"))
-				if err != nil && err != io.EOF {
-					log.WithError(err).Error("Failed to write the linefeed character on exit")
-					return
-				}
-
-				log.Info("Connection released, received EOF")
-				return
-			}
 			if err != nil && !err.(net.Error).Timeout() {
 				log.WithError(err).Error("Failed to read the connection buffer from socket")
+				sc.err = err
 				return
 			}
 			// exit on ctrl + q
 			if bytes.Contains(b[0:n], []byte{0x11}) {
 				return
 			}
+
 			if synchable, ok := param.remoteConsole.Stdout.(*os.File); ok {
 				if err := synchable.Sync(); err != nil {
 					log.Warn("Failed %v to flush %s", param.remoteConsole.Stdout, err)
 				}
 			}
 			_, err = param.remoteConsole.Stdout.Write(b[0:n])
-			if err != nil {
+			if err != nil{
 				log.WithError(err).Error("Failed to write to the console stdout buffer")
+				sc.err = err
 				return
 			}
 		}
 	}
+}
+
+func (sc *SerialConnection) Error() string {
+	return fmt.Sprintf("Serial console failed: %v", sc.err)
+}
+
+func (sc *SerialConnection) ExitCode() int {
+	if sc.err != nil && != sc.err != io.EOF {
+		return 1
+	}
+	return 0
 }
 
 func (sc *SerialConnection) AttachSerialConsole(param *hypervisor.ConsoleParam, waitClosed *sync.WaitGroup, errc chan error) error {
@@ -139,6 +140,11 @@ func (sc *SerialConnection) AttachSerialConsole(param *hypervisor.ConsoleParam, 
 	go func() {
 		<-serialConsoleParam.closeChan
 		close(finished)
+
+		_, err := serialConsoleParam.remoteConsole.Stdout.Write([]byte{0x0A})
+		if err != nil && err != io.EOF {
+			log.WithError(err).Warn("Failed to write the linefeed character on exit")
+		}
 	}()
 
 	return nil
