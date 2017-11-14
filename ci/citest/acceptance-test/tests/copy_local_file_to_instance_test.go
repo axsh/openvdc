1e1851aa59cde26564ac12c1ea7afa79089e566c
b0r6 <b0r6@users.noreply.github.com>
2017-11-14 17:20:27 +0900

    gofmt

--- ci/citest/acceptance-test/tests/copy_local_file_to_instance_test.go
+++ ci/citest/acceptance-test/tests/copy_local_file_to_instance_test.go
@@ -6,49 +6,46 @@ import (
 	"fmt"
 	"io/ioutil"
 	"path/filepath"
-        "strings"
-        "testing"
-        "time"
+	"strings"
+	"testing"
+	"time"
 )
 
 func TestCopyLocalFileToInstance(t *testing.T) {
-        stdout, _ := RunCmdAndReportFail(t, "openvdc", "run", "centos/7/lxc", `{"interfaces":[{"type":"veth"}], "node_groups":["linuxbr"]}`)
-        instance_id := strings.TrimSpace(stdout.String())
+	stdout, _ := RunCmdAndReportFail(t, "openvdc", "run", "centos/7/lxc", `{"interfaces":[{"type":"veth"}], "node_groups":["linuxbr"]}`)
+	instance_id := strings.TrimSpace(stdout.String())
 
-        WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
-        RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, "sudo lxc-info -n "+instance_id, 10, 5)
+	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
+	RunSshWithTimeoutAndReportFail(t, executor_lxc_ip, "sudo lxc-info -n "+instance_id, 10, 5)
 
-
-
-	testFileContent  :=  "Hi, this is a test."
-	testFileFolder	 :=  "/tmp/"
-	testFileName	 :=  "testfile"
-	testFilePath     :=  filepath.Join(testFileFolder, testFileName)
+	testFileContent := "Hi, this is a test."
+	testFileFolder := "/tmp/"
+	testFileName := "testfile"
+	testFilePath := filepath.Join(testFileFolder, testFileName)
 
 	err := ioutil.WriteFile(testFilePath, []byte(testFileContent), 0644)
 	if err != nil {
 		t.Error(err)
 	}
-	RunCmdAndReportFail(t, "openvdc", "copy", testFilePath,  instance_id +":"+ testFileFolder)
+	RunCmdAndReportFail(t, "openvdc", "copy", testFilePath, instance_id+":"+testFileFolder)
 	copiedFile := filepath.Join("/var/lib/lxc/", instance_id, "/rootfs/", testFilePath)
-        stdout, _, err = RunSsh(executor_lxc_ip, fmt.Sprintf("sudo cat %s", copiedFile))
-        if err != nil {
-                t.Error(err)
-        }
-        if stdout.Len() == 0 {
-                t.Errorf("Couldn't read %s", copiedFile)
-        }
+	stdout, _, err = RunSsh(executor_lxc_ip, fmt.Sprintf("sudo cat %s", copiedFile))
+	if err != nil {
+		t.Error(err)
+	}
+	if stdout.Len() == 0 {
+		t.Errorf("Couldn't read %s", copiedFile)
+	}
 	if stdout.String() != testFileContent {
-		t.Errorf("Content mismatch. Got: %s, expected: %s", stdout.String() ,testFileContent)
+		t.Errorf("Content mismatch. Got: %s, expected: %s", stdout.String(), testFileContent)
 	}
 
+	_, _ = RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "stop", instance_id)
+	t.Log("Waiting for instance to become STOPPED...")
+	WaitInstance(t, 5*time.Minute, instance_id, "STOPPED", []string{"RUNNING", "STOPPING"})
 
-        _, _ = RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "stop", instance_id)
-        t.Log("Waiting for instance to become STOPPED...")
-        WaitInstance(t, 5*time.Minute, instance_id, "STOPPED", []string{"RUNNING", "STOPPING"})
+	_, _ = RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
 
-        _, _ = RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
-
-        t.Log("Waiting for instance "+instance_id+" to become TERMINATED...")
-        WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
+	t.Log("Waiting for instance " + instance_id + " to become TERMINATED...")
+	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
 }
