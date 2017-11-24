e5bc54058aafbc953578296046309947867a2069
itouri <k-akiyama@axsh.net>
2017-11-24 15:00:49 +0900

    Adding switch function

--- model/store.go
+++ model/store.go
@@ -3,6 +3,7 @@ package model
 import (
 	"fmt"
 
+	log "github.com/Sirupsen/logrus"
 	_ "github.com/mesos/mesos-go/detector/zoo"
 	mesos "github.com/mesos/mesos-go/mesosproto"
 	util "github.com/mesos/mesos-go/mesosutil"
@@ -12,23 +13,52 @@ import (
 var storedOffers map[string]*mesos.Offer = make(map[string]*mesos.Offer)
 
 func IsThereSatisfidCreateReq(i *Instance) (bool, error) {
-	if instanceResource, ok := i.ResourceTemplate().(InstanceResource); ok {
-		cpus := instanceResource.GetVcpu()
-		mem := instanceResource.GetMemoryGb()
 
-		for _, offer := range storedOffers {
-			offeredCpus := getOfferScalar(offer, "vcpu")
-			if int32(offeredCpus) > cpus {
-				offeredMem := getOfferScalar(offer, "mem")
-				if int32(offeredMem) > mem {
-					return true, nil
-				}
+	var instanceResource InstanceResource
+	if instanceResource, ok := i.ResourceTemplate().(InstanceResource); !ok {
+		//FIXME instanceResource declared and not used
+		return false, fmt.Errorf("Templete do not have vcpu and mem", instanceResource)
+	}
+
+	// TODO: Avoid type switch to find template types.
+	var offerParams []string
+	var reqValues []int32
+	switch t := i.GetTemplate().GetItem(); t.(type) {
+	case *Template_Lxc:
+		offerParams = []string{"vcpu", "mem"}
+		reqValues = []int32{
+			instanceResource.GetVcpu(),
+			instanceResource.GetMemoryGb(),
+		}
+	case *Template_Null:
+
+	case *Template_Qemu:
+		offerParams = []string{"vcpu", "mem"}
+		reqValues = []int32{
+			instanceResource.GetVcpu(),
+			instanceResource.GetMemoryGb(),
+		}
+	case *Template_Esxi:
+
+	default:
+		log.Warnf("Unknown template type: %T", t)
+		return true, fmt.Errorf("Unknown template type: %T", t)
+	}
+
+	for _, offer := range storedOffers {
+		var index int
+		for i, param := range offerParams {
+			index = i
+			offerValue := int32(getOfferScalar(offer, param))
+			if offerValue < reqValues[i] {
+				break
 			}
 		}
-		return true, nil
+		if index == len(offerParams)-1 {
+			return true, nil
+		}
 	}
-	//DEBUG Return true in temporary to pass the other test
-	return true, fmt.Errorf("Templete do not have vcpu and mem")
+	return false, nil
 }
 
 func StoreOffer(offer *mesos.Offer) {
