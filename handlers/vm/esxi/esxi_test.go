ae7bfc72e7a6ab871f624aa475cbd816ad71774d
toros11 <tobias.rosen@axsh.net>
2017-12-12 23:28:41 +0900

    Fix some tests that i forgot to update

--- handlers/vm/esxi/esxi_test.go
+++ handlers/vm/esxi/esxi_test.go
@@ -1,23 +1,23 @@
 package esxi
 
 import (
-        "bytes"
-        "testing"
+	"bytes"
+	"testing"
 
-        "github.com/axsh/openvdc/handlers"
-        "github.com/axsh/openvdc/model"
-        "github.com/stretchr/testify/assert"
+	"github.com/axsh/openvdc/handlers"
+	"github.com/axsh/openvdc/model"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestResourceName(t *testing.T) {
-        assert := assert.New(t)
-        assert.Equal("vm/esxi", handlers.ResourceName(&EsxiHandler{}))
+	assert := assert.New(t)
+	assert.Equal("vm/esxi", handlers.ResourceName(&EsxiHandler{}))
 }
 
 func TestTypes(t *testing.T) {
-        assert := assert.New(t)
-        assert.Implements((*handlers.ResourceHandler)(nil), &EsxiHandler{})
-        assert.Implements((*handlers.CLIHandler)(nil), &EsxiHandler{})
+	assert := assert.New(t)
+	assert.Implements((*handlers.ResourceHandler)(nil), &EsxiHandler{})
+	assert.Implements((*handlers.CLIHandler)(nil), &EsxiHandler{})
 }
 
 const jsonEsxiImage = `{
@@ -29,14 +29,14 @@ const jsonEsxiImage = `{
 }`
 
 func TestEsxiHandler_ParseTemplate(t *testing.T) {
-        assert := assert.New(t)
-        h := &EsxiHandler{}
-        m, err := h.ParseTemplate(bytes.NewBufferString(jsonEsxiImage).Bytes())
-        assert.NoError(err)
-        assert.IsType((*model.EsxiTemplate)(nil), m)
-        modelesxi := m.(*model.EsxiTemplate)
-        assert.NotNil(modelesxi.GetEsxiImage())
+	assert := assert.New(t)
+	h := &EsxiHandler{}
+	m, err := h.ParseTemplate(bytes.NewBufferString(jsonEsxiImage).Bytes())
+	assert.NoError(err)
+	assert.IsType((*model.EsxiTemplate)(nil), m)
+	modelesxi := m.(*model.EsxiTemplate)
+	assert.NotNil(modelesxi.GetEsxiImage())
 
-        assert.Equal(modelesxi.GetEsxiImage().GetTemplate().GetName(), "sample")
-	    assert.Equal(modelesxi.GetEsxiImage().GetTemplate().GetDatastore(), "datastore")
+	assert.Equal(modelesxi.GetEsxiImage().GetName(), "sample")
+	assert.Equal(modelesxi.GetEsxiImage().GetDatastore(), "datastore")
 }
