46c4e62e46a4c89b2dbfa0084bfdce6617bb995c
toros11 <tobias.rosen@axsh.net>
2017-12-13 17:07:10 +0900

    Use the Timeout function and quote cmd to prevent word splitting

--- ci/citest/acceptance-test/tests/set_gateway_test.go
+++ ci/citest/acceptance-test/tests/set_gateway_test.go
@@ -24,8 +24,12 @@ func TestDefaultGatewayLXC(t *testing.T) {
 	stdout, _ := RunCmdAndReportFail(t, "openvdc", "run", "centos/7/lxc", tmpl)
 	instance_id := strings.TrimSpace(stdout.String())
 	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
+
 	// todo: find a way that does not rely on the console command for this (ssh?)
-	RunCmdAndReportFail(t, "sh", "-c", fmt.Sprintf("openvdc console %s ping -c 1 -W 3 8.8.8.8", instance_id))
+	stdout, _ = RunCmdWithTimeoutAndReportFail(t, 10, 5, "sh", "-c",
+		fmt.Sprintf("openvdc console %s '%s'", instance_id, "ping -c 1 -W 3 8.8.8.8"))
+	t.Log("\n", stdout.String())
+
 	RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
 	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
 }
@@ -34,8 +38,12 @@ func TestDefaultGatewayQEMU(t *testing.T) {
 	stdout, _ := RunCmdAndReportFail(t, "openvdc", "run", "centos/7/qemu_ga", tmpl)
 	instance_id := strings.TrimSpace(stdout.String())
 	WaitInstance(t, 10*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
+
 	// todo: find a way that does not rely on the console command for this (ssh?)
-	RunCmdAndReportFail(t, "sh", "-c", fmt.Sprintf("openvdc console %s ping -c -W 3 1 8.8.8.8", instance_id))
+	stdout, _ = RunCmdWithTimeoutAndReportFail(t, 10, 5, "sh", "-c",
+		fmt.Sprintf("openvdc console %s '%s'", instance_id, "ping -c 1 -W 3 8.8.8.8"))
+	t.Log("\n", stdout.String())
+
 	RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
 	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
 }
