bf3e06a056a269c47290df137bb131768b7a328f
toros11 <tobias.rosen@axsh.net>
2017-12-14 22:06:04 +0900

    Move tests

--- ci/citest/acceptance-test/tests/02_start_and_terminate_lxc_instance_test.go
+++ ci/citest/acceptance-test/tests/02_start_and_terminate_lxc_instance_test.go
@@ -86,3 +86,18 @@ func TestLXCInstance_OvsNICx2(t *testing.T) {
 	RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
 	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
 }
+
+func TestLXCInstance_DefaultGateway(t *testing.T) {
+	stdout, _ := RunCmdAndReportFail(t, "openvdc", "run", "centos/7/lxc",
+		`{"interfaces":[{"type":"veth","ipv4addr": "172.16.10.100","gateway": "172.16.10.1"}], "node_groups":["linuxbr"]}`)
+	instance_id := strings.TrimSpace(stdout.String())
+	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
+
+	// todo: find a way that does not rely on the console command for this (ssh?)
+	stdout, _ = RunCmdWithTimeoutAndReportFail(t, 10, 5, "sh", "-c",
+		fmt.Sprintf("openvdc console %s '%s'", instance_id, "ping -c 1 -W 3 10.16.90.199"))
+	t.Log("\n", stdout.String())
+
+	RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
+	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
+}
--- ci/citest/acceptance-test/tests/03_start_and_terminate_qemu_instance_test.go
+++ ci/citest/acceptance-test/tests/03_start_and_terminate_qemu_instance_test.go
@@ -93,3 +93,18 @@ func TestQEMUInstance_OVSBrNICx2(t *testing.T) {
 	RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
 	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
 }
+
+func TestQEMUInstance_DefaultGateway(t *testing.T) {
+	stdout, _ := RunCmdAndReportFail(t, "openvdc", "run", "centos/7/lxc",
+		`{"interfaces":[{"type":"veth","ipv4addr": "172.16.10.100","gateway": "172.16.10.1"}], "node_groups":["linuxbr"]}`)
+	instance_id := strings.TrimSpace(stdout.String())
+	WaitInstance(t, 10*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
+
+	// todo: find a way that does not rely on the console command for this (ssh?)
+	stdout, _ = RunCmdWithTimeoutAndReportFail(t, 10, 5, "sh", "-c",
+		fmt.Sprintf("openvdc console %s '%s'", instance_id, "ping -c 1 -W 3 10.16.90.199"))
+	t.Log("\n", stdout.String())
+
+	RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
+	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
+}
