cfb61d6d6bb4094f23ed91a8c99f43e33aa9ff8b
b0r6 <b0r6@users.noreply.github.com>
2017-12-19 18:37:32 +0900

    gofmt

--- ci/citest/acceptance-test/tests/force_destroy_test.go
+++ ci/citest/acceptance-test/tests/force_destroy_test.go
@@ -9,16 +9,16 @@ import (
 )
 
 func TestForceDestroy(t *testing.T) {
-        stdout, _ := RunCmdAndReportFail(t, "openvdc", "run", "centos/7/lxc", `{"interfaces":[{"type":"veth"}], "node_groups":["linuxbr"]}`)
-        instance_id := strings.TrimSpace(stdout.String())
+	stdout, _ := RunCmdAndReportFail(t, "openvdc", "run", "centos/7/lxc", `{"interfaces":[{"type":"veth"}], "node_groups":["linuxbr"]}`)
+	instance_id := strings.TrimSpace(stdout.String())
 
-        WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
+	WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
 
 	//Simulate stuck state
 	RunCmdAndReportFail(t, "openvdc", "force-state", instance_id, "starting")
-        WaitInstance(t, 1*time.Minute, instance_id, "STARTING", nil)
+	WaitInstance(t, 1*time.Minute, instance_id, "STARTING", nil)
 
-        _, _ = RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", "--force", instance_id)
-        t.Log("Waiting for instance " + instance_id + " to become TERMINATED...")
-        WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
+	_, _ = RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", "--force", instance_id)
+	t.Log("Waiting for instance " + instance_id + " to become TERMINATED...")
+	WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
 }
