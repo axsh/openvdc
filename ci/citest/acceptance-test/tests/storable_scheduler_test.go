7eb9d11f31680a9b9462b75bbfa3e75afb6ba5d3
itouri <k-akiyama@axsh.net>
2017-12-25 18:49:27 +0900

    Modify function name miss

--- ci/citest/acceptance-test/tests/storable_scheduler_test.go
+++ ci/citest/acceptance-test/tests/storable_scheduler_test.go
@@ -6,16 +6,9 @@ import (
 	"testing"
 )
 
-func TestStorable_scheduler_NullTemplate(t *testing.T) {
+func TestStorable_scheduler_LxcHugeTemplate(t *testing.T) {
 	_, stderr := RunCmdAndReportFail(t, "openvdc", "run", "centos/7/lxc_huge")
 	if stderr.String() != "There is no machine can satisfy resource requirement" {
 		t.Error("There is no machine can satisfy resource requirement but work")
 	}
-
-	// instance_id := strings.TrimSpace(stdout.String())
-
-	// WaitInstance(t, 5*time.Minute, instance_id, "RUNNING", []string{"QUEUED", "STARTING"})
-
-	// RunCmdWithTimeoutAndReportFail(t, 10, 5, "openvdc", "destroy", instance_id)
-	// WaitInstance(t, 5*time.Minute, instance_id, "TERMINATED", nil)
 }
