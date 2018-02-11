65a14ac012c0e6893d57bf93630fc6c014c7e27a
itouri <k-akiyama@axsh.net>
2018-02-11 20:17:14 +0900

    Modified LxcHugeTemplate test to expect fail

--- ci/citest/acceptance-test/tests/storable_scheduler_test.go
+++ ci/citest/acceptance-test/tests/storable_scheduler_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestStorable_scheduler_LxcHugeTemplate(t *testing.T) {
-	_, stderr := RunCmdAndReportFail(t, "openvdc", "run", "centos/7/lxc_huge")
+	_, stderr := RunCmdAndExpectFail(t, "openvdc", "run", "centos/7/lxc_huge")
 	if stderr.String() != "There is no machine can satisfy resource requirement" {
 		t.Error("There is no machine can satisfy resource requirement but work")
 	}
